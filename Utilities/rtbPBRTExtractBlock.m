be5fa5aa78405a0b9f2e303f2ab8412586da0faf
Trisha Lian <trisha.lian@oculus.com>
2017-10-13 12:08:20 -0700

    Split block extraction to another function.

---
+++ Utilities/rtbPBRTExtractBlock.m
@@ -0,0 +1,41 @@
+function blockLines = rtbPBRTExtractBlock(txtLines,varargin)
+% Given a series of text lines in cell format (e.g. output of rtbPBRTRead),
+% extract a block that corresponds to the given keyword.
+
+% Example
+% txtLines = rtbPBRTRead('/home/wandell/pbrt-v2-spectral/pbrt-scenes/sanmiguel.pbrt');
+% cameraBlock = rtbPBRTExtractBlock(txtLines,'blockName','camera')
+
+% TL Scienstanford 2017
+
+
+%%
+p = inputParser;
+p.addRequired('txtLines',@(x)(iscell(txtLines) && ~isempty(txtLines)));
+addParameter(p,'blockName','Camera',@ischar);
+p.parse(txtLines,varargin{:});
+
+blockName = p.Results.blockName;
+
+%% Extract lines that correspond to specified keyword
+
+nLines = length(txtLines);
+for ii=1:nLines
+    thisLine = txtLines{ii};
+    if length(thisLine) >= length(blockName)
+        if strncmp(thisLine,blockName,length(blockName))
+            blockBegin = ii;
+            for jj=(ii+1):nLines
+                if isempty(txtLines{jj})
+                    blockEnd = jj;
+                    break;
+                end
+            end
+        end
+    end
+end
+
+blockLines = txtLines(blockBegin:(blockEnd-1));
+
+
+end
