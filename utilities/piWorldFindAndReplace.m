5db5670c0dfc6390ad6c0c514945af189b641ba3
Trisha Lian <trisha.lian@oculus.com>
2017-11-20 11:29:51 -0800

    Added textured plane scene along with script to show how to use it.

---
+++ utilities/piWorldFindAndReplace.m
@@ -0,0 +1,55 @@
+function recipe = piWorldFindAndReplace(recipe,oldString,newString,varargin)
+%
+% Syntax
+%   recipe = piFindAndReplace(recipe,oldString,newString)
+%
+% Find a replace a given string. We only do this within the World block. 
+%
+% Input
+%   recipe:        a recipe object that includes a WorldBegin/WorldEnd block.
+%   oldString:     String to find
+%   newString:     String to replace it with
+%
+% Optional parameter/values
+%   Nothing yet...
+%
+% Return
+%    recipe - the same recipe but with an updated WorldBegin/WorldEnd
+%             block.
+%
+% TL Scien Stanford 2017
+
+%%
+p = inputParser;
+p.addRequired('recipe',@(x)isequal(class(x),'recipe'));
+p.addRequired('oldString',@(x)ischar(x));
+p.addRequired('newString',@(x)ischar(x)); 
+
+p.parse(recipe,oldString,newString,varargin{:});
+     
+%% Look for the old string within the world block
+
+world = recipe.world;
+
+foundCount = 0;
+for ii = 1:length(world)
+    currLine = world{ii};
+    if(contains(currLine,oldString))
+        
+        foundCount = foundCount + 1;
+        currLine = strrep(currLine,oldString,newString);
+        world{ii} = currLine;
+        
+    end
+end
+
+if(foundCount == 0)
+    warning('Did not find object: %s \n',objectName);
+else
+    fprintf('Found and replaced %i instances. \n',foundCount);
+end
+
+% Update world
+recipe.world = world;
+
+end
