d1eb70af14484ab22684e45414f40d0da7f69fb4
Trisha Lian <trishalian@oculus.stanford.edu>
2017-11-21 17:41:02 -0800

    Added warning about cleaning up the living room scene.

--- scripts/pbrtV3/s_pbrtv3Example.m
+++ scripts/pbrtV3/s_pbrtv3Example.m
@@ -14,6 +14,10 @@ if ~piDockerExists, piDockerConfig; end
 % https://benedikt-bitterli.me/resources/
 % or the direct link here:
 % https://benedikt-bitterli.me/resources/pbrt-v3/living-room-2.zip
+
+% WARNING: You will have to "clean up" the pbrt file before running, or
+% else the parser will not read it correctly. Soon we will put up a cleaned
+% up version of this scene somewhere, but right now it's too big. 
 recipe = piRead('/Users/trishalian/GitRepos/pbrt-v3-scenes-Bitterli/living-room-2/scene.pbrt','version',3);
 
 %% Change the camera lens
