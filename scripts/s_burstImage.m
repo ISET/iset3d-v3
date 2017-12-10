c43efb125e415420c788c09376f65b6dc95973fb
Brian Wandell <wandell@stanford.edu>
2017-12-09 16:52:59 -0800

    Updated but still not working.

--- scripts/s_burstImage.m
+++ scripts/s_burstImage.m
@@ -26,9 +26,8 @@ from = thisR.get('from');
 %% Default is a relatively low resolution (256).
 
 thisR.set('camera','pinhole');
-thisR.set('from',from + [0 0 100]);  % First left/right, 2nd moved camera closer and to the right 
-thisR.set('film resolution',256);
-thisR.set('rays per pixel',128);
+thisR.set('film resolution',128);
+thisR.set('rays per pixel',64);
 
 %% Set up Docker 
 
@@ -38,23 +37,36 @@ nShots = 4;
 FOV    = 10;
 sensor = sensorCreate;
 sensor = sensorSet(sensor,'fov',FOV);
+sensor = sensorSet(sensor,'exp time',0.008);  % 8 ms exposure
+
+% These are the positions of the camera for each shot
 dFrom = [0 0 25; 0 0 30; 0 0 60; 0 0 100];
+
 for ii=1:nShots
     n = sprintf('%s-%d',n,ii);
-    thisR.outputFile = fullfile(piRootPath,'local','chessLF',[n,e]);
+    thisR.outputFile = fullfile(piRootPath,'local','chessBurst',[n,e]);
     thisR.set('from',from + dFrom(ii,:));  % First left/right, 2nd moved camera closer and to the right 
-    workingDir = piWrite(thisR);    
+    workingDir = piWrite(thisR);  
+    
+    % At this point, we could end the loop and do the calculation in
+    % the cloud for all of the nShots
+    % end
+    
+    % Then we would retrieve the dat files that were rendered and
+    % proceed from here  
     oi = piRender(thisR);
+    %
+    
     oi = oiSet(oi,'fov',FOV);
     sensor = sensorCompute(sensor,oi);
     volts = sensorGet(sensor,'volts');
     vImage = fullfile(workingDir,'renderings',[n,'.png']);
     imwrite(volts,vImage,'png');
 end
+
 % Show it in ISET
 vcAddObject(oi); oiWindow;
 
 %% Create a matched light field sensor
-
 
 vcAddObject(sensor); sensorWindow;
