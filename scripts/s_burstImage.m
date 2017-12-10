445fe8773ab2929e3de8c5b8a4011d11e54c71d1
Brian Wandell <wandell@stanford.edu>
2017-12-10 10:34:55 -0800

    Updates for rays per pixel and looking good on burst image

--- scripts/s_burstImage.m
+++ scripts/s_burstImage.m
@@ -19,14 +19,23 @@ if ~piDockerExists, piDockerConfig; end
 % fname = fullfile(piRootPath,'data','ChessSet','chessSet.pbrt');
 fname = fullfile(piRootPath,'data','teapot-area','teapot-area-light.pbrt');
 if ~exist(fname,'file'), error('File not found'); end
+[~,basename,ext] = fileparts(fname); 
 
+% Working directory
+workDir = fullfile(piRootPath,'local','teapot');
 % Read the main scene pbrt file.  Return it as a recipe
 thisR = piRead(fname);
 from = thisR.get('from');
 
 FOV = thisR.get('fov');
 
+
 %% Set up Docker 
+
+% thisR.get('rays per pixel')
+thisR.set('rays per pixel',32);
+
+
 % Chess set
 % thisR.set('camera','pinhole');
 % thisR.set('from',from + [0 0 100]);  % First left/right, 2nd moved camera closer and to the right 
@@ -34,26 +43,29 @@ FOV = thisR.get('fov');
 % thisR.set('rays per pixel',128);
 
 %%
-[~,n,e] = fileparts(fname); 
 
-nShots = 12;
-eTime  = 0.002;
+nShots = 6;
+eTime  = 0.004;
 sensor = sensorCreate;
 sensor = sensorSet(sensor,'fov',FOV);
 sensor = sensorSet(sensor,'exp time',0.008);  % 8 ms exposure
+vSwing = sensorGet(sensor,'pixel voltage swing');
 
 % These are the positions of the camera for each shot
 % We need a convenient way to express the parameters in terms of angles or
 % distance or something
-x = randn(nShots,1)*0.5;
+x = randn(nShots,1)*0.3;
 dFrom = zeros(nShots,3);
 dFrom(:,1) = x;
 
 for ii=1:nShots
-    n = sprintf('%s-%d',n,ii);
-    thisR.outputFile = fullfile(piRootPath,'local','teapot',[n,e]);
-    thisR.set('from',from + dFrom(ii,:));  % First left/right, 2nd moved camera closer and to the right
-    if ii == 1, workingDir = piWrite(thisR);
+    outputName = sprintf('%s-%d%s',basename,ii,ext);
+    thisR.outputFile = fullfile(workDir,outputName);
+    
+    % First left/right, 2nd moved camera closer and to the right
+    thisR.set('from',from + dFrom(ii,:)); 
+    
+    if ii == 1, piWrite(thisR);
     else,       piWrite(thisR,'overwrite resources',false);
     end
     
@@ -83,8 +95,11 @@ for ii=1:nShots
         voltSum = voltSum + sensorGet(sensor,'volts');
     end
     
-    vImage = fullfile(workingDir,'renderings',[n,'.png']);
-    imwrite(volts,vImage,'png');
+    outputName = sprintf('%s-%d%s',basename,ii,'.png');
+    vImage = fullfile(workingDir,'renderings',outputName);
+
+    % Write out png scaled to 0 to 255, re: voltage swing
+    imwrite((volts/vSwing)*255,vImage,'png');
 end
 
 %% Create a matched light field sensor
