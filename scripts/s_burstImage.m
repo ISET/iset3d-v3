b0759cdff8ef7130d3d76487d8ef5077e6890d92
Brian Wandell <wandell@stanford.edu>
2017-12-09 23:24:03 -0800

    Adjustments for burst imaging mode.

--- scripts/s_burstImage.m
+++ scripts/s_burstImage.m
@@ -16,57 +16,97 @@ if ~piDockerExists, piDockerConfig; end
 
 % We organize the pbrt files with its includes (textures, brdfs, spds, geometry)
 % in a single directory. 
-fname = fullfile(piRootPath,'data','ChessSet','chessSet.pbrt');
+% fname = fullfile(piRootPath,'data','ChessSet','chessSet.pbrt');
+fname = fullfile(piRootPath,'data','teapot-area','teapot-area-light.pbrt');
 if ~exist(fname,'file'), error('File not found'); end
 
 % Read the main scene pbrt file.  Return it as a recipe
 thisR = piRead(fname);
 from = thisR.get('from');
 
-%% Default is a relatively low resolution (256).
-
-thisR.set('camera','pinhole');
-thisR.set('film resolution',128);
-thisR.set('rays per pixel',64);
+FOV = thisR.get('fov');
 
 %% Set up Docker 
+% Chess set
+% thisR.set('camera','pinhole');
+% thisR.set('from',from + [0 0 100]);  % First left/right, 2nd moved camera closer and to the right 
+% thisR.set('film resolution',256);
+% thisR.set('rays per pixel',128);
 
+%%
 [~,n,e] = fileparts(fname); 
 
-nShots = 4;
-FOV    = 10;
+nShots = 12;
+eTime  = 0.002;
 sensor = sensorCreate;
 sensor = sensorSet(sensor,'fov',FOV);
 sensor = sensorSet(sensor,'exp time',0.008);  % 8 ms exposure
 
 % These are the positions of the camera for each shot
-dFrom = [0 0 25; 0 0 30; 0 0 60; 0 0 100];
+% We need a convenient way to express the parameters in terms of angles or
+% distance or something
+x = randn(nShots,1)*0.5;
+dFrom = zeros(nShots,3);
+dFrom(:,1) = x;
 
 for ii=1:nShots
     n = sprintf('%s-%d',n,ii);
-    thisR.outputFile = fullfile(piRootPath,'local','chessBurst',[n,e]);
-    thisR.set('from',from + dFrom(ii,:));  % First left/right, 2nd moved camera closer and to the right 
-    workingDir = piWrite(thisR);  
+    thisR.outputFile = fullfile(piRootPath,'local','teapot',[n,e]);
+    thisR.set('from',from + dFrom(ii,:));  % First left/right, 2nd moved camera closer and to the right
+    if ii == 1, workingDir = piWrite(thisR);
+    else,       piWrite(thisR,'overwrite resources',false);
+    end
     
     % At this point, we could end the loop and do the calculation in
     % the cloud for all of the nShots
     % end
     
     % Then we would retrieve the dat files that were rendered and
-    % proceed from here  
-    oi = piRender(thisR);
-    %
+    % proceed from here
+    scene = piRender(thisR,...
+        'renderType','radiance',...
+        'meanluminance',100,...
+        'fov',FOV);
+    % vcAddObject(scene); sceneWindow;
     
-    oi = oiSet(oi,'fov',FOV);
+    oi = oiCreate;
+    oi = oiCompute(oi,scene);
+    % vcAddObject(oi); oiWindow;
+    
+    sensor = sensorSet(sensor,'exp time',eTime);
     sensor = sensorCompute(sensor,oi);
-    volts = sensorGet(sensor,'volts');
+    % vcAddObject(sensor); sensorWindow;
+    if ii == 1
+        volts = sensorGet(sensor,'volts');
+        voltSum = volts;
+    else
+        voltSum = voltSum + sensorGet(sensor,'volts');
+    end
+    
     vImage = fullfile(workingDir,'renderings',[n,'.png']);
     imwrite(volts,vImage,'png');
 end
 
-% Show it in ISET
-vcAddObject(oi); oiWindow;
-
 %% Create a matched light field sensor
 
+% This is the blurred image
+sensorBurst = sensorSet(sensor,'volts',voltSum);
+sensorBurst = sensorSet(sensorBurst,'name','burst');
+vcAddObject(sensorBurst); sensorWindow;
+sensorSet(sensorBurst,'gamma',0.5);
+
+%%  Now a single shot from a still recording with the same total time.
+
+thisR.set('from',from);  % First left/right, 2nd moved camera closer and to the right
+piWrite(thisR,'overwrite resources',false);
+scene = piRender(thisR,...
+    'renderType','radiance',...
+    'meanluminance',100,...
+    'fov',FOV);
+oi = oiCompute(oi,scene);
+sensor = sensorSet(sensor,'exp time',nShots * eTime);
+sensor = sensorCompute(sensor,oi);
+sensor = sensorSet(sensor,'name','still');
 vcAddObject(sensor); sensorWindow;
+sensorSet(sensor,'gamma',0.5);
+%%
