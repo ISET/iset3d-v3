89ebca0e42ce189b9c43eda6c2451522b1e964a0
Trisha Lian <trishalian@oculus.stanford.edu>
2018-01-25 13:52:41 -0800

    Updates to process360oi script.

--- scripts/pbrtV3/360CameraSimulation/s_process360oi.m
+++ scripts/pbrtV3/360CameraSimulation/s_process360oi.m
@@ -13,33 +13,31 @@ if(~exist(outputDirectory,'dir'))
     mkdir(outputDirectory);
 end 
 
-%% Read first file to determine photon scale factor and fixed exposure time
-% TODO?
-
 %% Get scale factor to remove vignetting
 
-% Fish-eye
-% TODO...
+removeVignetting = 1;
 
 % Wide-angle
-wideAngleWhiteFile = fullfile(workingDir,'wideAngleWhiteScene.mat');
-if(exist(wideAngleWhiteFile,'file'))
-    whiteSceneOI = load(wideAngleWhiteFile);
-    whiteSceneOI = whiteSceneOI.oi;
-    whiteScenePhotons = oiGet(whiteSceneOI,'photons');
-    % whiteLevel = oiGet(whiteSceneOI,'illuminance');
-    
-    % Note we're getting some weird color artifacts near the edge of the
-    % white scene. I still need to debug this. But in the meantime, let's
-    % just use a middle wavelength.
-    nWaves = oiGet(whiteSceneOI,'nWaves');
-    whiteLevel = whiteScenePhotons(:,:,round(nWaves/2));
-    vignetteScale = 1./(whiteLevel./max(whiteLevel(:)));
-    
-    % Note: Rings are a consequence of the pupil sampling method used in PBRTv3.
-    % figure; imagesc(vignetteScale); colorbar;
-else
-    vignetteScale = 1;
+if(removeVignetting)
+    wideAngleWhiteFile = fullfile(workingDir,'wideAngleWhiteScene.mat');
+    if(exist(wideAngleWhiteFile,'file'))
+        whiteSceneOI = load(wideAngleWhiteFile);
+        whiteSceneOI = whiteSceneOI.oi;
+        whiteScenePhotons = oiGet(whiteSceneOI,'photons');
+        % whiteLevel = oiGet(whiteSceneOI,'illuminance');
+        
+        % Note we're getting some weird color artifacts near the edge of the
+        % white scene. I still need to debug this. But in the meantime, let's
+        % just use a middle wavelength.
+        nWaves = oiGet(whiteSceneOI,'nWaves');
+        whiteLevel = whiteScenePhotons(:,:,round(nWaves/2));
+        vignetteScale = 1./(whiteLevel./max(whiteLevel(:)));
+        
+        % Note: Rings are a consequence of the pupil sampling method used in PBRTv3.
+        % figure; imagesc(vignetteScale); colorbar;
+    else
+        vignetteScale = 1;
+    end
 end
       
 %% Loop through all images
@@ -51,7 +49,10 @@ originAll = [];
 targetAll = [];
 upAll = [];
 indicesAll = [];
-    
+
+% Read first file to determine photon scale factor
+
+
 for ii = 1:nFiles
     
     clear oi;
@@ -63,44 +64,16 @@ for ii = 1:nFiles
     
     % Instead of adjusting illuminance for each image, we scale the number
     % of photons by the same factor for all images on the rig. This keeps
-    % the scale across images on the rig relative. 
+    % the scale across images on the rig relative. You may have to play
+    % with the scale depending on the scene. 
     scale = 1e13; % This creates an mean illuminance of roughly 50 lux for cam1 for the whiteRoom
-    % scale = 1e11; % for livingroom
+    %scale = 1e11; % for livingroom
     photons = scale.*oiGet(oi,'photons');
     oi = oiSet(oi,'photons',photons);
     
-    % Sensor diagonal was set to be 16 mm in s_360CameraRig. We have to set
-    % that correctly in the optical image. 
-    % TODO: Set it correctly in the s_360CameraRig.m instead of here. 
-    
-    % Add oi specific parameters
-    if(strcmp(dirInfo(ii).name,'cam0.dat') || ...
-            strcmp(dirInfo(ii).name,'cam15.dat') || ...
-            strcmp(dirInfo(ii).name,'cam16.dat'))
-        % Fish eye lens
-         lensFocalLength = 6;
-         apertureDiameter = 6;
-         filmDiag = 16;
-    else
-        % Wide-angle
-        lensFocalLength = 6;
-        apertureDiameter = 0.87;
-        filmDiag = 16;
-    end
-    
-    oi = oiSet(oi, 'optics focal length', lensFocalLength * 1e-3);
-    oi = oiSet(oi,'optics fnumber',lensFocalLength/apertureDiameter);
-    
-    % Compute the horizontal field of view
-    photons = oiGet(oi, 'photons');
-    x = size(photons,2);
-    y = size(photons,1);
-    d = sqrt(x.^2 + y.^2);  % Number of samples along the diagonal
-    fwidth= (filmDiag / d) * x;    % Diagonal size by d gives us mm per step
-    fov = 2 * atan2d(fwidth / 2, lensFocalLength);
-    
-    % Store the horizontal field of view in degrees in the oi
-    oi = oiSet(oi, 'fov', fov);
+    % It's helpful at this point to check the dimensions of the OI given in the
+    % window. Are they reasonable? If not, it's possible the focal length,
+    % aperture diameter, and FOV were not set correctly when saving the OI.
    
     % --- Setup sensor ---
     
@@ -112,7 +85,7 @@ for ii = 1:nFiles
     sensorPixelSize = oiGet(oi,'sample spacing','m');
     oiHeight = oiGet(oi,'height');
     oiWidth = oiGet(oi,'width');
-    sensorSize = round([oiWidth oiHeight]./sensorPixelSize);
+    sensorSize = round([oiHeight oiWidth]./sensorPixelSize);
     sensor = sensorSet(sensor,'size',sensorSize);
     sensor = sensorSet(sensor,'pixel size same fill factor',sensorPixelSize);
     
@@ -134,20 +107,22 @@ for ii = 1:nFiles
     % --- Setup Image Processing ---
     ip = ipCreate;
     ip = ipSet(ip,'demosaic method','bilinear');
-    ip = ipSet(ip,'correction method illuminant','none');
+    ip = ipSet(ip,'correction method illuminant','gray world');
     
     % Compute!
     ip = ipCompute(ip,sensor);
     
-    % Scale according to the white image (remove vignetting)
-    % Is there anything built in to ISET to do this?
-    if(strcmp(dirInfo(ii).name,'cam0.dat') || ...
-            strcmp(dirInfo(ii).name,'cam15.dat') || ...
-            strcmp(dirInfo(ii).name,'cam16.dat'))
-        % Fish eye lens
-    else
-        % Wide-angle
-        ip.data.result = ip.data.result.*vignetteScale;
+    if(removeVignetting)
+        % Scale according to the white image (remove vignetting)
+        % Is there anything built in to ISET to do this?
+        if(strcmp(dirInfo(ii).name,'cam0.dat') || ...
+                strcmp(dirInfo(ii).name,'cam15.dat') || ...
+                strcmp(dirInfo(ii).name,'cam16.dat'))
+            % Fish eye lens
+        else
+            % Wide-angle
+            ip.data.result = ip.data.result.*vignetteScale;
+        end
     end
     
     vcAddObject(ip);
@@ -158,15 +133,28 @@ for ii = 1:nFiles
     % Flip the indexing. The cameras should run clockwise, but from PBRT
     % they run counter clockwise. I think this is due to some coordinate
     % axes flips.
+    
     allIndices = [0 circshift(14:-1:1,1) 15 16];
     expression = '(\d+)';
     matchStr = regexp(dirInfo(ii).name,expression,'match');
     currIndex = str2double(cell2mat(matchStr));
     newIndex = allIndices(currIndex+1);
     
+%     expression = '(\d+)';
+%     matchStr = regexp(dirInfo(ii).name,expression,'match');
+%     currIndex = str2double(cell2mat(matchStr));
+%     newIndex = currIndex;
     
     % Save the images according to the Surround360 format
     srgb = ipGet(ip,'data srgb');
+    
+    % Crop the image
+%     [M,N,C] = size(srgb);
+%     center = round([M/2,N/2]);
+%     removeX = 30;
+%     removeY = round(removeX*(M/N));
+%     srgb = srgb(removeY:(2*center(1)-removeY),removeX:(2*center(2)-removeX),:);
+    
     imageDir = fullfile(outputDirectory,sprintf('cam%d',newIndex));
     if(~exist(imageDir,'dir'))
         mkdir(imageDir);
@@ -177,15 +165,17 @@ for ii = 1:nFiles
     % We will save the origins/targets etc. according to the new index.
     % This is helpful when we try to match up with the camera_rig.json
     % file.
+    %{
     originAll = [originAll; origin];
     targetAll = [targetAll; target];
     upAll = [upAll; up];  
     indicesAll = [indicesAll; newIndex];
-    
+    %}
 end
 
-%% Output a json geometry rig file
 
+%% Output a json geometry rig file
+%{
 % Save camera rig geometry info
 % Maybe we should write this in a text file. 
 save(fullfile(outputDirectory,'cameraRigGeometry.mat'),'originAll','targetAll','upAll','indicesAll','rigOrigin');
@@ -249,4 +239,4 @@ end
 
 opts = struct('indent',' ');
 jsonwrite(fullfile(workingDir,'camera_rig_initial.json'),rig,opts);
-
+%}
