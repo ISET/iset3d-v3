c82551a96389205f8e7b8a6e4986c923973a163d
Zhenyi Liu <zhenyiliu@stanford.edu>
2018-02-21 14:20:34 -0800

    now s_pbrtv3Example_material.m is avalible for testing material modification

--- utilities/piWriteMaterial.m
+++ utilities/piWriteMaterial.m
@@ -1,4 +1,4 @@
-function piWriteMaterial(materials,txtLines,output)
+function piWriteMaterial(thisR)
 % find cells except for NamedMaterial
 
 %% Parse the output file, working directory, stuff like that.
@@ -6,6 +6,8 @@ function piWriteMaterial(materials,txtLines,output)
 %%
 % Empty any line that contains MakeNamedMaterial
 % The remaining lines have a texture definition.
+output = thisR.outputFile;
+txtLines = thisR.txtLines;
 for i = 1:size(txtLines)
     if ~isempty(txtLines(i))
         if contains(txtLines(i),'MakeNamedMaterial')
@@ -16,12 +18,13 @@ end
 
 % Squeeze out the empty lines????
 % Some day we might get the parsed textures here.
-% textureLines = squeeze(txtLines);
+textureLines = txtLines(~cellfun('isempty',txtLines));
 
 %% Create txtLines for the material struct array
-materialTxt = cell(1,length(materials));
-for ii=1:length(materials)
-  materialTxt{ii} = piMaterialText(materials(ii)); % function that converts the struct to text
+
+materialTxt = cell(1,length(thisR.materials));
+for ii=1:length(thisR.materials)
+  materialTxt{ii} = piMaterialText(thisR.materials(ii)); % function that converts the struct to text
 end
 
 %% Combine txtLines that are textures and defined materials
@@ -31,15 +34,103 @@ end
 
 fileID = fopen(output,'w');
 
-for row=1:size(txtLines)
-    fprintf(fileID,'%s\n',txtLines{row});
+for row=1:size(textureLines)
+    fprintf(fileID,'%s\n',textureLines{row});
 end
 
 % Add the materials
-for row=1:size(materiallist)
+for row=1:length(materialTxt)
     fprintf(fileID,'%s\n',materialTxt{row});
 end
 
 fclose(fileID);
+fileparts(output)
+fprintf('%s are overwritten successfully \n', fileparts(output));
+end
 
+%% function that converts the struct to text
+function val = piMaterialText(materials)
+val_name = sprintf('MakeNamedMaterial "%s" ',materials.name);
+val = val_name;
+val_string = sprintf('"string type" "%s" ',materials.string);
+val = strcat(val, val_string);
+
+if ~isempty(materials.floatindex) 
+    val_floatindex = sprintf('"float index" [%0.5f] ',materials.floatindex);
+    val = strcat(val, val_floatindex);
+end
+
+if ~isempty(materials.texturekd)
+    val_texturekd = sprintf('"texture Kd" "%s" ',materials.texturekd);
+    val = strcat(val, val_texturekd);
+end
+
+if ~isempty(materials.texturekr)
+    val_texturekr = sprintf('"texture Kr" "%s" ',materials.texturekr);
+    val = strcat(val, val_texturekr);
+end
+
+if ~isempty(materials.textureks)
+    val_textureks = sprintf('"texture Ks" "%s" ',materials.textureks);
+    val = strcat(val, val_textureks);
+end
+
+if ~isempty(materials.rgbkr)
+    val_rgbkr = sprintf('"rgb Kr" [%0.5f %0.5f %0.5f] ',materials.rgbkr);
+    val = strcat(val, val_rgbkr);
+end
+
+if ~isempty(materials.rgbks)
+    val_rgbks = sprintf('"rgb Ks" [%0.5f %0.5f %0.5f] ',materials.rgbks);
+    val = strcat(val, val_rgbks);
+end
+
+if ~isempty(materials.rgbkd)
+    val_rgbkd = sprintf('"rgb Kd" [%0.5f %0.5f %0.5f] ',materials.rgbkd);
+    val = strcat(val, val_rgbkd);
+end
+
+if ~isempty(materials.colorkd)
+    val_colorkd = sprintf('"color Kd" [%0.5f %0.5f %0.5f] ',materials.colorkd);
+    val = strcat(val, val_colorkd);
+end
+
+if ~isempty(materials.colorks)
+    val_colorks = sprintf('"color Ks" [%0.5f %0.5f %0.5f] ',materials.colorks);
+    val = strcat(val, val_colorks);
+end
+
+if ~isempty(materials.floaturoughness)
+    val_floaturoughness = sprintf('"float uroughness" [%0.5f] ',materials.floaturoughness);
+    val = strcat(val, val_floaturoughness);
+end
+
+if ~isempty(materials.floatvroughness)
+    val_floatvroughness = sprintf('"float vroughness" [%0.5f] ',materials.floatvroughness);
+    val = strcat(val, val_floatvroughness);
+end
+
+if ~isempty(materials.floatroughness)
+    val_floatroughness = sprintf('"float roughness" [%0.5f] ',materials.floatroughness);
+    val = strcat(val, val_floatroughness);
+end
+
+if ~isempty(materials.spectrumkd)
+    val_spectrumkd = sprintf('"spectrum Kd" "%s" ',materials.spectrumkd);
+    val = strcat(val, val_spectrumkd);
+end
+
+if ~isempty(materials.spectrumks)
+    val_spectrumks = sprintf('"spectrum Ks" "%s" ',materials.spectrumks);
+    val = strcat(val, val_spectrumks);
+end
+
+if ~isempty(materials.stringnamedmaterial1)
+    val_stringnamedmaterial1 = sprintf('"string namedmaterial1" "%s" ',materials.stringnamedmaterial1);
+    val = strcat(val, val_stringnamedmaterial1);
+end
+if ~isempty(materials.stringnamedmaterial2)
+    val_stringnamedmaterial2 = sprintf('"string namedmaterial2" "%s" ',materials.stringnamedmaterial2);
+    val = strcat(val, val_stringnamedmaterial2);
+end
 end
