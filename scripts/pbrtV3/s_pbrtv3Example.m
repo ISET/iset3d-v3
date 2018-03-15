c968092f4551c35415bb16662cecdbac9c30d7e5
tlian7 <tlian@stanford.edu>
2018-03-15 13:21:04 -0700

    Added more compatibility with pbrt-v3-spectral.

--- scripts/pbrtV3/s_pbrtv3Example.m
+++ scripts/pbrtV3/s_pbrtv3Example.m
@@ -3,13 +3,16 @@
 % TL SCIEN 2017
 
 %% Initialize ISET and Docker
-
-% Check: Does the pbrt-v3-spectral docker container pull automatically?
 ieInit;
 if ~piDockerExists, piDockerConfig; end
 
 %% Read the file
 recipe = piRead(fullfile(piRootPath,'data','V3','teapot','teapot-area-light.pbrt'),'version',3);
+
+%% Add a realistic camera
+recipe.set('camera','realistic');
+recipe.set('lensfile',fullfile(piRootPath,'data','lens','dgauss.22deg.50.0mm.dat'));
+recipe.set('filmdiagonal',35); 
 
 %% Change render quality
 recipe.set('filmresolution',[128 128]);
@@ -17,15 +20,14 @@ recipe.set('pixelsamples',128);
 recipe.set('maxdepth',1); % Number of bounces
 
 %% Render
-% ~ 6 seconds on an 8 core machine
-
+% ~ 20 seconds on an 8 core machine
 oiName = 'teapotTest';
 recipe.set('outputFile',fullfile(piRootPath,'local',strcat(oiName,'.pbrt')));
 
 piWrite(recipe);
-[scene, result] = piRender(recipe);
+[oi, result] = piRender(recipe);
 
 ieAddObject(scene);
 sceneWindow;
 
-scene = sceneSet(oi,'gamma',0.5);
+oi = oiSet(oi,'gamma',0.5);
