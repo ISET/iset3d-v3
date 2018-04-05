5f9dab92e409648a616bc6b09c4ac6a616806bfc
tlian7 <tlian@stanford.edu>
2018-04-04 17:06:46 -0700

    Added new function to create a "point" scene as well as script to estimate PSF using said scene.

---
+++ scripts/pbrtV3/s_psfEstimate.m
@@ -0,0 +1,55 @@
+%% Demonstrate how to estimate a PSF using "piCreateSimplePointScene."
+%
+% Here we approximate the PSF by rendering a very small circular disk at a
+% certain distance away from the camera. The disk it completely white, so
+% it should reflect the illumination of the scene. The illumination can be
+% specified using 'illumination.'
+%
+% Another way to do this is to make an RGB image with a single pixel in the
+% center, and to place this on a textured plane some distance from the
+% camera using s_texturedPlane.
+
+% TL SCIEN 2017
+
+%% Initialize ISET and Docker
+
+ieInit;
+if ~piDockerExists, piDockerConfig; end
+
+workingFolder = fullfile(piRootPath,'local','psfEstimate');
+
+%% Create a simple point scene
+
+% Warning: If you change the illumination spectrum, you must put a copy of
+% the .spd file into the working folder! The default illumination spectrum
+% is D65.spd. 
+recipe = piCreateSimplePointScene(...
+    'pointDistance',3,...
+    'pointDiameter',0.01,...
+    'illumination','D65.spd',...
+    'pbrtVersion',3);
+
+%% Change image resolution
+
+recipe.set('filmresolution',[256 256]);
+recipe.set('pixelsamples',256);
+
+%% Add a camera
+% Currently we cannot (efficientally) render chromatic aberration for
+% PBRTv3, so this will be monochromatic.
+
+recipe.set('camera','realistic');
+recipe.set('lensfile',fullfile(piRootPath,'data','lens','dgauss.22deg.50.0mm.dat'));
+recipe.set('filmdiagonal',10); 
+
+%% Write and render
+
+oiName = 'psfTest';
+recipe.set('outputFile',fullfile(workingFolder,strcat(oiName,'.pbrt')));
+
+piWrite(recipe);
+[oi, results] = piRender(recipe);
+
+vcAddAndSelectObject(oi);
+oiWindow;
+
---
+++ utilities/piCreateSimplePointScene.m
@@ -0,0 +1,87 @@
+function recipe = piCreateSimplePointScene(varargin)
+%CREATESIMPLEPOINT 
+% Create a recipe for a simple point scene given some scene parameters. 
+
+% OPTIONAL input parameter/val
+%   pointDiameter - diameter of the point (meters)
+%   pointRGB - color of the point, as an rgb value
+%   pointSpectrum - spectrum of the point, as an SPD filename. If a
+%       spectrum is given, it will take precedence over the RGB value.
+%   pointDistance - distance away from the origin along the positive z-axis
+%   illumination - illumination of the scene (infinite light) as as SPD
+%       filename.
+%   pbrtVersion - version number
+
+% RETURN
+%   recipe - recipe for this created scene
+
+%% Parse inputs
+parser = inputParser();
+parser.addParameter('pointDiameter',0.01, @isnumeric);
+parser.addParameter('pointRGB', [], @isvector);
+parser.addParameter('pointSpectrum', '', @isstring);
+parser.addParameter('pointDistance',1, @isnumeric);
+parser.addParameter('illumination', 'D65.spd', @ischar);
+parser.addParameter('pbrtVersion', 3, @isnumeric);
+
+parser.parse(varargin{:});
+pointRGB = parser.Results.pointRGB;
+pointSpectrum = parser.Results.pointSpectrum;
+illumination = parser.Results.illumination;
+pbrtVersion = parser.Results.pbrtVersion;
+
+% If version 2, the units are in millimeters. If version 3, units are in
+% meters. We will scale the units appropriately. 
+if(pbrtVersion == 2)
+    unitScale = 10^3;
+elseif(pbrtVersion ==3)
+    unitScale = 1;
+else
+    error('PBRT version not recognized.');
+end
+
+pointDiameter = parser.Results.pointDiameter*unitScale;
+pointDistance = parser.Results.pointDistance*unitScale;
+
+% Warn users of unimplemented features
+if(~(isempty(pointRGB)) || ~isempty(pointSpectrum))
+    warning('pointRGB and pointSpectrum are not implemented yet. Point RGB set to [1 1 1] by default.');
+end
+%% Read in base scene
+
+if(pbrtVersion == 3)
+    recipe = piRead(fullfile(piRootPath,'data','SimplePoint','simplePointV3.pbrt'),'version',3);
+elseif(pbrtVersion == 2)
+    recipe = piRead(fullfile(piRootPath,'data','SimplePoint','simplePoint.pbrt'),'version',2);
+
+end
+%% Make adjustments to the point
+
+% Clear previous transforms
+piClearObjectTransforms(recipe,'Point');
+
+% Add given transforms
+piObjectTransform(recipe,'Point','Scale',[pointDiameter pointDiameter 1]);
+piObjectTransform(recipe,'Point','Translate',[0 0 pointDistance]);
+
+%% Make adjustments to the background plane
+
+piClearObjectTransforms(recipe,'Plane');
+
+% Make it large!
+piObjectTransform(recipe,'Plane','Scale',[pointDistance*10 pointDistance*10 1]);
+
+% Move it slightly beyond the point
+piObjectTransform(recipe,'Plane','Translate',[0 0 pointDistance+0.5*unitScale]);
+
+%% Make adjustments to color
+
+% TODO: Ask Zhenyi about this...
+
+%% Make adjustments to the light
+
+recipe = piWorldFindAndReplace(recipe,'D65.spd',illumination);
+
+
+end
+
