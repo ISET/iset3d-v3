6b5cf1fc86beea7ebe11984e6b1e41c5dfbe4cad
tlian7 <tlian@stanford.edu>
2018-04-12 17:11:57 -0700

    Added scale factor for optical image output. Included an example script.

--- scripts/pbrtV3/s_psfEstimate.m
+++ scripts/pbrtV3/s_psfEstimate.m
@@ -42,7 +42,7 @@ recipe.set('camera','realistic');
 recipe.set('lensfile',fullfile(piRootPath,'data','lens','dgauss.22deg.50.0mm.dat'));
 recipe.set('filmdiagonal',10); 
 
-recipe.set('focus distance',3);
+recipe.set('focus distance',1);
 recipe.set('aperture diameter',5);
 
 %% Write and render
