d106209bf2a8b8a7c68bcc950b08bc7b2d20c375
tlian7 <tlian@stanford.edu>
2018-05-28 19:31:18 -0700

    Clean up/update of all sceneEye tutorial scripts.

--- tutorials/sceneEye/t_vergenceAccomm.m
+++ tutorials/sceneEye/t_vergenceAccomm.m
@@ -1,6 +1,9 @@
 %% t_vergenceAccomm.m
+% 
+% We can do a simple simulation of vergence and accommodation by having the
+% eyes converge and accommodate to a "moving" red sphere.
 %
-% Depends on: pbrt2ISET, ISETBIO, Docker
+% Depends on: iset3d, isetbio, Docker, RemoteDataToolbox
 %
 % TL ISETBIO Team, 2017
 
@@ -12,52 +15,63 @@ myScene = sceneEye('chessSet');
 
 %% Set parameters
 
-myScene.resolution = 128; 
-myScene.numRays = 128;
+myScene.resolution = 256; 
+myScene.numRays = 256;
 
 ipd = 64e-3; % Average interpupillary distance
 
+% We save the original parameters so we have them around even after moving
+% and rotating the eye. 
 originalPos = myScene.eyePos;
-originalWorld = myScene.recipe.world;
-startingPos = originalPos + [0 0 0.1];
+startingPos = originalPos + [0 0 0.1]; % This position looks a little better.
 
-% oi = myScene.render;
-% 
-% ieAddObject(oi);
-% oiWindow;
+% Needed because we will objects to the 3d world. This text block contains
+% the "original" scene.
+originalWorld = myScene.recipe.world; 
 
 %% Create binocular retinal images
 
-vergenceZ = [-0.3 0.0 0.2]; % Along z-axis
+% We'll move the red sphere to these distances along the z-axis (optical
+% axis).
+sphereZ = [-0.2 0.0 0.2]; % Along z-axis
 
-for ii = 1:length(vergenceZ)
+for ii = 1:length(sphereZ)
     
-    vergencePoint = [0 startingPos(2) vergenceZ(ii)];
+    % [x y z]
+    % We want the sphere to be at same height as the eye but at different
+    % distances away from it.
+    spherePos = [0 startingPos(2) sphereZ(ii)];
     
+    % Move the two eyes apart
     leftEyePos = startingPos - [ipd/2 0 0];
     rightEyePos = startingPos + [ipd/2 0 0];
     
-    myScene.eyeTo = vergencePoint;
+    % Make them point at the sphere
+    myScene.eyeTo = spherePos;
     
     % Reset world (Important!)
+    % We want to start from the original, vanilla version of the 3d scene
+    % with no sphere.
     myScene.recipe.world = originalWorld;
     
-    % Add the target sphere
+    % Add the red sphere
     myScene.recipe = piAddSphere(myScene.recipe,...
         'rgb',[1 0 0],...
         'radius',0.005,...
-        'location',vergencePoint);
+        'location',spherePos);
     
-    % Set accommodation to the right distance
-    dist = sqrt(sum((vergencePoint -leftEyePos).^2)); % in mm
+    % Accommodate to the sphere
+    dist = sqrt(sum((spherePos -leftEyePos).^2)); % in mm
     myScene.accommodation = 1/dist;
     
+    % Render the left eye
     myScene.eyePos = leftEyePos;
-    myScene.name = sprintf('leftEye_%0.2fm',vergenceZ(ii));
-    oi = myScene.render;
+    myScene.name = sprintf('leftEye_%0.2fm',sphereZ(ii));
+    [oi, result] = myScene.render;
     vcAddAndSelectObject(oi);
     oiWindow;
     
+    % Render the right eye
     myScene.eyePos = rightEyePos;
     myScene.name = sprintf('rightEye%0.2fm',vergenceZ(ii));
     oi = myScene.render;
