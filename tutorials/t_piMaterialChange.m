c887aa0b9cc3845c03737864d687deeadc285342
Zhenyi-Liu <zhenyiliu@coronal.stanford.edu>
2018-06-13 16:30:47 -0700

    Add option for createmateral when write out a pbrt scene

--- tutorials/t_piMaterialChange.m
+++ tutorials/t_piMaterialChange.m
@@ -21,7 +21,7 @@ thisR = piRead(fname,'version',3);
 % [800 600] 32 - takes around 30 seconds to render on a machine with 8 cores.
 % [300 150] 16 -
 
-thisR.set('filmresolution',[800 600]/2);
+thisR.set('filmresolution',[800 600]);
 thisR.set('pixelsamples',16);
 
 %% List material library
@@ -32,31 +32,10 @@ thisR.set('pixelsamples',16);
 fprintf('A library of materials\n\n');  % Needs a nicer print method
 disp(thisR.materials.lib)
 
-%% Set up the material
-
 % This value determines the number of bounces.  To have glass we need
 % to have at least 2 or more.  We start with only 1 bounce, so it will
 % not appear like glass or mirror.
 thisR.integrator.maxdepth.value = 4;
-
-% For this scene, the BODY material is attached to ???? object.  We
-% need to parse the geometry file to make sure.  This will happen, but
-% has not yet.
-%{
-mList = fieldnames(thisR.materials.list);
-for ii=1:length(mList)
-    fprintf('%d %s\n',ii,mList{ii});
-end
-R = input('Which material:  ','s');
-material = mList{str2num(R)};
-%}
-
-material = 'BODY';      % A type of material.
-target = thisR.materials.lib.carpaint;  % Give it a chrome spd
-rgbkd  = [1 0 1];                          % Make it red diffuse reflection
-rgbkr  = [0.753 0.753 0.753];              % Specularish in the different channels
-
-piMaterialAssign(thisR,material,target,'rgbkd',rgbkd,'rgbkr',rgbkr);
 
 %% Write out the pbrt scene file, based on thisR.
 [p,n,e] = fileparts(fname); 
@@ -67,7 +46,7 @@ piWrite(thisR);
 
 %% Render
 tic, scene = piRender(thisR); toc
-scene = sceneSet(scene,'name','Glass off');
+scene = sceneSet(scene,'name',sprintf('Glass on (%d)',thisR.integrator.maxdepth.value));
 ieAddObject(scene); sceneWindow;
 
 %% Change the sphere to glass
@@ -79,14 +58,15 @@ target = thisR.materials.lib.plastic;    % Give it a chrome spd
 rgbkr  = [0.5 0.5 0.5];              % Reflection
 rgbkd  = [0.5 0.5 0.5];              % Scatter
 
-piMaterialAssign(thisR, 'pyramid', target,'rgbkd',rgbkd,'rgbkr',rgbkr);
-
-piWrite(thisR);
+piMaterialAssign(thisR, 'GLASS', target,'rgbkd',rgbkd,'rgbkr',rgbkr);
+[p,n,e] = fileparts(fname); 
+thisR.set('outputFile',fullfile(piRootPath,'local','SimpleSceneExport',[n,'1',e]));
+piWrite(thisR,'creatematerials',true);
 
 %% Render again
 
 tic, scene = piRender(thisR); toc
-scene = sceneSet(scene,'name',sprintf('Glass on (%d)',thisR.integrator.maxdepth.value));
+scene = sceneSet(scene,'name','Glass off');
 ieAddObject(scene); sceneWindow;
 
 %%
