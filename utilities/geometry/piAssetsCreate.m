5b8d31be73af9824426716bd806392a87bfed436
Brian Wandell <wandell@stanford.edu>
2018-07-09 10:46:38 -0700

    ZL and BW inserted some scitran calls

--- utilities/geometry/piAssetsCreate.m
+++ utilities/geometry/piAssetsCreate.m
@@ -1,17 +1,41 @@
-function obj = piAssetsCreate(thisR, varargin)
-% Add 
+function assets = piAssetsCreate(thisR, varargin)
+% Create and combine assets using base information from a recipe
+%
+% Inputs
+%  thisR - A rendering recipe
+%
+% Optional key/value parameters
+%   nCars
+%   nTrucks
+%   nPeople
+%   nBuses
+%   nCyclist
+%   scitran
+%
+% Returns
+%   assets - Struct with the asset geometries and materials
+%
+% Zhenyi, Vistasoft Team, 2018
+
+%%
 p = inputParser;
-p.addRequired('renderRecipe',@(x)isequal(class(x),'recipe'));
+
+p.addRequired('thisR',@(x)isequal(class(x),'recipe'));
 p.addParameter('nCars',1);
 p.addParameter('nTrucks',0);
 p.addParameter('nPeople',0);
 p.addParameter('nBuses',0);
 p.addParameter('nCyclist',0);
+p.addParameter('scitran','',@(x)(isa(x,'scitran')));
 
-p.parse(varargin{:});
+p.parse(thisR,varargin{:});
 inputs = p.Results;
+st = p.Results.scitran;
+if isempty(st), st = scitran('stanfordlabs'); end
 
-obj = [];
+hierarchy = st.projectHierarchy('Computer Graphics','project id','5b3eac08d294db0016bb1a2b');
+% projects = st.search('project','project label exact','Computer Graphics')
+assets = [];
 cnt = 1;
 
 %% Cars
@@ -20,7 +44,7 @@ cnt = 1;
 % if the cars required are less then what we have on the flywheel. 
 % Obj Instance should be used, but better with different color for carbody. 
 for ii=1:inputs.nCars
-    
+
     currentClass = 'car';
     nClass = length(assets.(currentClass));
     
@@ -28,7 +52,22 @@ for ii=1:inputs.nCars
     objects(cnt).id = randi(nClass);
     % Download a random car obj from flywheel car session
     
-    % list Car session
+    % list the acquisition labels in the first session
+    whichSession = 1;
+    stPrint(hierarchy.acquisitions{whichSession},'label','')
+    file = st.search('file',...
+        'session label exact','spaceship',...
+        'acquisition label contains','Car',...
+        'file name contains','obj');
+    fname     = fullfile(stRootPath,'local','myCar.obj');
+    localFile = st.fileDownload(file{1},'destination',fname);
+    
+    % Example of how to randomly select acquisitions from a specific
+    % session
+    whichSession = 1;
+    nAcqs = length(hierarchy.acquisitions{whichSession});
+    nSamples = 2;
+    r = randi(nAcqs,[nSamples,1]);   % Column veftor of random integers
     
     % Choose one aquisition or mutiple? zip 
     % desDir folder or file?
@@ -36,8 +75,8 @@ for ii=1:inputs.nCars
     thisR = piRead(filename,'version',3);
     piMaterialGroupAssign(thisR);
     geometry = piGeometryRead(thisR);
-    obj(ii).material =thisR.materials.list;
-    obj(ii).geometry = geometry;
+    assets(ii).material =thisR.materials.list;
+    assets(ii).geometry = geometry;
     cnt = cnt+1;
 end
 
