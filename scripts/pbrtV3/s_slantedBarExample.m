d68519f92fbc50abc5734b3bb0a05952091b30d3
Trisha Lian <trisha.lian@oculus.com>
2018-08-03 15:01:52 -0700

    Added adjustable slanted bar scene.

---
+++ scripts/pbrtV3/s_slantedBarExample.m
@@ -0,0 +1,55 @@
+%% s_slantedBarExample
+
+% Render a slanted bar with the two sides at two different depths.
+
+% TL SCIEN 2017
+
+%% Initialize ISET and Docker
+
+ieInit;
+if ~piDockerExists, piDockerConfig; end
+
+workingFolder = fullfile(piRootPath,'local','slantedBarExample');
+
+%% Create the slanted bar scene
+% Depth is in meters.
+recipe = piCreateSlantedBarScene(...
+    'whiteDepth',1,...
+    'blackDepth',2,...
+    'illumination','EqualEnergy.spd');
+
+%% Change image resolution
+recipe.set('filmresolution',[128 128]);
+recipe.set('pixelsamples',256);
+
+%% Add a camera
+recipe.set('camera','realistic');
+recipe.set('lensfile',fullfile(piRootPath,'data','lens','dgauss.22deg.50.0mm.dat'));
+recipe.set('filmdiagonal',10); 
+
+recipe.set('focus distance',1);
+recipe.set('aperture diameter',5);
+
+%% Write and render
+
+oiName = 'slantedBarExample';
+recipe.set('outputFile',fullfile(workingFolder,strcat(oiName,'.pbrt')));
+
+piWrite(recipe);
+[oi, results] = piRender(recipe);
+
+vcAddAndSelectObject(oi);
+oiWindow;
+
+%% Check the depth map
+% There is a slight mismatch between the given depth and the set depth.
+% This is due to the distance between the back of the lens and the front of
+% the camera. This discrepency has been removed in isetBio/sceneEye since
+% it makes a big difference when doing analysis related with the eye.
+
+depthMap = oiGet(oi,'depthMap');
+figure();
+imagesc(depthMap); colorbar;
+axis off; axis image;
+
+
---
+++ tutorials/sceneEye/t_occlusionExample.m
@@ -0,0 +1,74 @@
+%% t_occlusionExample.m
+%
+% Render a slanted bar where the black plane is at a different depth from
+% the white plane. This creates an edge where there is a depth
+% discontinuity.
+%
+% Depends on: pbrt2ISET, ISETBIO, Docker, ISET
+%
+% TL ISETBIO Team, 2018
+
+%% Initialize ISETBIO
+ieInit;
+if ~piDockerExists, piDockerConfig; end
+
+%% Try different depths
+whiteDepthDpt = 5; % Always fixed at 5 dpt
+blackDepthDpt = [4 5 6]; % Vary this
+whiteDepth = 1/whiteDepthDpt;
+blackDepth = 1./blackDepthDpt;
+
+% Note: On a 4 core laptop, each image takes around 30 seconds to render
+% with the following settings. In total, the script takes around 1.5
+% minutes to run.
+
+depthMapsFig = figure();
+k = 1; % For subplots on figure
+
+for ii = 1:length(blackDepth)
+    
+    % Create the scene
+    % Scene is lit by EqualEnergy.spd by default.
+    scene3d = sceneEye('slantedBarAdjustable',...
+        'whiteDepth',whiteDepth,...
+        'blackDepth',blackDepth(ii)); % in meters
+    
+    % Set eye parameters
+    scene3d.accommodation = whiteDepthDpt; % Accommodate to white plane
+    scene3d.numCABands = 6; % Can increase to 16 or 32 at the cost of render speed. 
+    scene3d.numBounces = 3;
+    
+    % Set size parameters
+    scene3d.fov = 2; % The smaller the fov the more the LCA is visible.
+    scene3d.resolution = 128; % Pretty LQ
+    scene3d.numRays = 256; % Pretty LQ
+    
+    % Render
+    scene3d.name = sprintf('%0.2f_%0.2f_slantedBar',...
+        whiteDepthDpt,blackDepthDpt(ii));
+    [oi, result] = scene3d.render;
+    
+    ieAddObject(oi);
+    oiWindow;
+    
+    % Put a figure together
+    depthMap = oiGet(oi,'depthMap');
+    rgb = oiGet(oi,'rgb');
+    
+    figure(depthMapsFig);
+    subplot(2,length(blackDepthDpt),k);
+    imshow(rgb);
+    title(sprintf('%0.1f dpt vs %0.1f dpt',...
+        blackDepthDpt(ii),whiteDepthDpt))
+    
+    subplot(2,length(blackDepthDpt),k+length(blackDepthDpt));
+    imagesc(depthMap,[0 max(blackDepth)]); 
+    colormap(gray);
+    h = colorbar; axis off; axis image;
+    ylabel(h, 'meters')
+    title(sprintf('%0.1f dpt vs %0.1f dpt ',...
+        blackDepthDpt(ii),whiteDepthDpt))
+    k = k+1;
+    
+    
+end
