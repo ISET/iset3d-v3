c935827e63bc09bcd74b858df7e7e511aa9d8b1a
tlian7 <tlian@stanford.edu>
2018-11-27 16:23:22 -0800

    Added check for cell matrices in piContains

--- utilities/piContains.m
+++ utilities/piContains.m
@@ -1,4 +1,4 @@
-function tf = pipiContains(str,pattern)
+function tf = piContains(str,pattern)
 % Returns 1 (true) if str contains pattern, and returns 0 (false) otherwise.
 %
 % Synopsis:
@@ -9,6 +9,16 @@ function tf = pipiContains(str,pattern)
 %    work with Matlab versions prior to those with piContains().
 %
 % See also: contains, strfind
+
+% Sometimes the str is a cell. This is no good for isempty. Let's check and
+% fix, if possible.
+if(iscell(str))
+    if((length(str) == 1))
+        str = str{1};
+    else
+        error('String given to piContains is a cell matrix with multiple entries.');
+    end
+end
 
 if (~isempty(strfind(str,pattern)))
     tf = 1;
