213ac338a767fc5eeb806704171764d8f3f232e9
Zhenyi Liu <zhenyi27@holidayfun.c.primal-surfer-140120.internal>
2019-01-12 11:04:44 -0800

    Save changes on metadata resize

--- utilities/sceneAuto/piBBox2dDraw.m
+++ utilities/sceneAuto/piBBox2dDraw.m
@@ -1,18 +1,35 @@
 function annotationFig=piBBox2dDraw(ieObject)
-% ieObject is an Optical Image
-% later it should be an oi
+% ieObject is a vcimage
 annotationFig = figure;
-pngFigure = oiGet(ieObject,'rgb image');
+pngFigure = ipGet(ieObject,'data srgb');
+ipWindow
+% pngFigure = xyz2rgb(pngFigure,'ColorSpace','adobe-rgb-1998');
 imshow(pngFigure);
 
 if ~isempty(ieObject.metadata.bbox2d)
     fds = fieldnames(ieObject.metadata.bbox2d);
     for kk = 1:length(fds)
         detections = ieObject.metadata.bbox2d.(fds{kk});
-        r = rand; g = rand; b = rand;
-        if r< 0.2 && g < 0.2 && b< 0.2
-            r = 0.5; g = rand; b = rand;
+        switch fds{kk}
+            case 'car'
+                r = 0.1; g= 0.8; b = 0.1;
+            case 'truck'
+                r = 0.1; g= 0.5; b = 0.1;
+            case 'pedestrian'
+                r = 0.8; g= 0.1; b = 0.1;
+            case 'bicycle'
+                r = 0.6; g= 0.3; b = 0.5;
+            case 'rider'
+                r = 0.7; g= 0.3; b = 0.1;
+            case 'motor'
+                r = 0.5; g= 0.7; b = 0.1;
+            case 'bus'
+                r = 0.5; g= 0.3; b = 0.7;
         end
+%         r = rand; g = rand; b = rand;
+%         if r< 0.2 && g < 0.2 && b< 0.2
+%             r = 0.5; g = rand; b = rand;
+%         end
         for jj=1:length(detections)
             if ~detections{jj}.ignore
                 pos = [detections{jj}.bbox2d.xmin detections{jj}.bbox2d.ymin ...
