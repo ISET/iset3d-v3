2992e794671afe47cd17a3617f47ad57f2d0b651
Zhenyi-Liu <zhenyiliu@stanford.edu>
2019-03-25 12:18:17 -0700

    update functions -new

--- utilities/sceneAuto/piBuildingPosList.m
+++ utilities/sceneAuto/piBuildingPosList.m
@@ -61,13 +61,13 @@ for kk = 1:length(objects.assets)
         switch type
             case 'front'
                 if coordination(1)<0
-                    ankor = coordination + [lenx_tmp, 0];
+                    ankor = coordination + [lenx_tmp, 1];
                 else
                     ankor = coordination;
                 end
             case 'back'
                 if coordination(1)>0
-                    ankor = coordination - [lenx_tmp, 0];
+                    ankor = coordination - [lenx_tmp, 1];
                 else
                     ankor = coordination;
                 end
@@ -166,7 +166,7 @@ end
 
 function [settle_list, count] = buildingPlan(building_list, lenx_tmp, ...
     leny_tmp, coordination, settle_list, count, type, ankor, aveW)
-offset = 2; % adjust the interval berween the buildings. Don't too big! Or might cause problem!
+offset = 0.2; % adjust the interval berween the buildings. Don't too big! Or might cause problem!
 
 %% calculate the parameter in spare region.
 switch type
