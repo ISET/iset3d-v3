5a2f1e11440ba96860a9056065df562d491db47c
Brian Wandell <wandell@stanford.edu>
2019-06-10 16:56:41 -0700

    Short help tutorial for inserting microlens into an imaging file

--- tutorials/microlens/t_piMicrolens.m
+++ tutorials/microlens/t_piMicrolens.m
@@ -3,11 +3,10 @@
 % Requires the isetlens toolbox
 %
 % See also
-%
+%   piCameraInsertMicrolens
 %
 
 %% 
-
 ieInit
 piDockerConfig;
 
@@ -20,12 +19,20 @@ imagingLensName = 'dgauss.22deg.3.0mm.json';
 imagingLens = lensC('filename','dgauss.22deg.3.0mm.json');
 imagingLens.draw;
 
-%%
+%% Matlab wrapper for the insert microlens docker tool
+%
 combinedLens = piCameraInsertMicrolens(microLensName,imagingLensName);
 
-%%
+%% Help command for the lenstool insertmicrolens
+%
+% Copy and paste this into a terminal window
+%
+docker run -ti --rm vistalab/pbrt-v3-spectral lenstool 
+
+%% Example of a docker command for the lenstool insertmicrolens
+%
+% Copy and paste this into a terminal window - after putting the
+% relevant files into place ...
+%
 docker run -ti --rm vistalab/pbrt-v3-spectral lenstool insertmicrolens -xdim 64 -ydim 64 dgauss.22deg.3.0mm.json microlens.2um.Example.json combined.json
-
-
-docker run -ti --rm vistalab/pbrt-v3-spectral lenstool insertmicrolens
 
