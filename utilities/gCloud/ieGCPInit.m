5e824c41dd9fab985db2cd98a59c785985ba3f53
Brian Wandell <wandell@stanford.edu>
2019-11-17 17:01:18 -0800

    Separate out the GCP initialization

---
+++ utilities/gCloud/ieGCPInit.m
@@ -0,0 +1,38 @@
+%% ieGCPInit
+%
+% Initialize the local environment for running on the GCP platform
+%
+% Checks that Docker, gCloud and scitran are on your path.
+% Initializes.
+%
+% Zheng, Brian 2019
+
+%%  Initialize ISETcam
+ieInit;
+
+% We need Docker
+if ~piDockerExists, piDockerConfig; end
+
+% The isetcloud toolbox must be on your path
+if ~mcGcloudExists, mcGcloudConfig; end
+
+%% Open a connection to the Flywheel scitran site
+
+% The scitran toolbox must be on your path
+st = scitran('stanfordlabs');
+
+%% Initialize the GCP cluster
+
+tic
+gcp = gCloud('configuration','cloudRendering-pbrtv3-west1-custom-50-56320-flywheel');
+
+toc
+gcp.renderDepth = 1;  % Create the depth map
+gcp.renderMesh  = 1;  % Create the object mesh for subsequent use
+gcp.targets     = []; % clear job list
+
+%% Print out the gcp parameters for the user if they want it.
+
+str = gcp.configList;
+
+%% END
