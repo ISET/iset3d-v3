9dd2b6d728adbd74846f9d3aa808a3fa8e4b8a93
wandell <wandell@stanford.edu>
2019-11-29 16:57:47 -0800

    Commenting and formatting

--- utilities/isetcam/piOI2IP.m
+++ utilities/isetcam/piOI2IP.m
@@ -1,24 +1,57 @@
 function [ip,sensor]=piOI2IP(oi,varargin)
+% Convert an OI to the IP state, carrying along the metadata
+%
+% Syntax
+%    [ip,sensor]=piOI2IP(oi,varargin)
+%
+% Description
+%   After we simulate the OI we have both the radiance and the pixel level
+%   metadata.  This function converts the OI and metadata all the way to
+%   the IP level.
+%
+% Input
+%   oi - This OI needs to have the metadata attached to it.
+%
+% Optional key/value pairs
+%   sensor - The name of a file containing the sensor
+%   pixel size - Size in microns (e.g. 2)
+%   film diagonal - In millimeters, default is 10 mm
+%
+% Output
+%   ip
+%   sensor
+%
+% See also
+%   piMetadataSetSize
+
+%%
 p = inputParser;
 varargin = ieParamFormat(varargin);
-p.addParameter('sensor','default');
-p.addParameter('pixelSize',[]);
+
+p.addParameter('sensor','default');   % A file name
+p.addParameter('pixelsize',[]);
 p.addParameter('filmdiagonal',10); % [mm]
+
 p.parse(varargin{:});
 sensorName   = p.Results.sensor;
-pixelSize= p.Results.pixelSize;
+pixelSize    = p.Results.pixelsize;
 filmDiagonal = p.Results.filmdiagonal;
+
 %% oi to sensor
 if strcmp(sensorName,'default')
     sensor = sensorCreate;
 else
     load(sensorName,'sensor');
 end
+
+% Not sure why these aren't settable.  I think they are here to conform
+% with the ISETAuto generalization paper
 readnoise  = 1e-3;
 darkvoltage= 1e-3;
 [electrons,~] = iePixelWellCapacity(pixelSize*1e6);%
 converGain = 1/electrons;% voltage swing/electrons
 % converGain = 1/2^12;
+
 sensor = sensorSet(sensor,'pixel read noise volts',readnoise);
 sensor = sensorSet(sensor,'pixel voltage swing',1);
 sensor = sensorSet(sensor,'pixel dark voltage',darkvoltage);
@@ -34,7 +67,9 @@ end
 oiSize = oiGet(oi,'size');
 optimalPixel = sqrt(filmDiagonal^2/(oiSize(1)^2+oiSize(2)^2))*1e-3;
 sensor = sensorSet(sensor, 'size', oiGet(oi,'size')*optimalPixel/pixelSize);
+
 % sensor   = sensorSetSizeToFOV(sensor,oiGet(oi,'fov'));
+
 eTime  = autoExposure(oi,sensor,0.90,'video','center rect',rect,'videomax',1/60);
 fprintf('eT: %s ms \n',eTime*1000);
 sensor = sensorSet(sensor,'exp time',eTime);
@@ -54,12 +89,16 @@ ip = ipCreate;
 % Choose the likely set of signals the sensor will encounter
 ip = ipSet(ip,'conversion method sensor','MCC Optimized');
 ip = ipSet(ip,'illuminant correction method','gray world');
+
 % demosaics = [{'Adaptive Laplacian'},{'Bilinear'}];
 ip = ipSet(ip,'demosaic method','Adaptive Laplacian'); 
 ip = ipCompute(ip,sensor);
+
 % ipWindow(ip);
+
 if isfield(sensor,'metadata')
     ip.metadata = sensor.metadata;
     ip.metadata.eT = eTime;
 end
+
 end
