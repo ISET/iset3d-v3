7e17e6862e23dd002d7993e935c8a7770bdcd2b5
ZhengLyu <zhenglyu@silver.stanford.edu>
2020-02-07 10:55:07 -0800

    change wave range for fluorescent effect

--- tutorials/underDevelopment/t_piIntro_macbeth.m
+++ tutorials/underDevelopment/t_piIntro_macbeth.m
@@ -106,7 +106,7 @@ piWrite(thisR, 'overwritematerials', true);
 
 %% Render 
 thisDocker = 'vistalab/pbrt-v3-spectral:fluorescent';
-wave = 395:10:705;
+wave = 385:5:705;
 [scene, result] = piRender(thisR, 'dockerimagename', thisDocker,'wave',wave);
 scene = sceneSet(scene,'name',sprintf('%s',sceneName));
 
