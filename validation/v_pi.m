bd3f19e20b9d22946acb2df2d4c9e7d6c026d080
Brian Wandell <wandell@stanford.edu>
2020-02-18 11:21:34 -0800

    Start of master validation script

---
+++ validation/v_pi.m
@@ -0,0 +1,23 @@
+%% Gateway to validation scripts
+%
+% Some tutorials, some scripts, some other validation programs
+%
+% ZL,BW
+%
+% See also
+%
+
+%%  
+t_piIntro_macbeth;
+
+t_piIntro_macbeth_fluorescent;
+
+t_piIntro_macbeth_zmap;
+
+%%  Check that the scenes in the data directory still run
+
+v_piDataScenes
+
+%%
+
+
