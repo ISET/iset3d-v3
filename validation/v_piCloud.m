1909ffc8ce031223ca4468bbf7532dd01843855a
Brian Wandell <wandell@stanford.edu>
2020-06-10 17:09:19 -0700

    More validation

---
+++ validation/v_piCloud.m
@@ -0,0 +1,19 @@
+%% Validations involving Google Cloud
+%
+%
+
+t_piCameraArray
+
+t_piDrivingScene_demo_local
+
+t_piFlywheelRender
+
+t_piExample_SceneAssembly
+
+t_piFlywheelSimpleScene
+
+t_piCloud
+
+t_piAcq2IP
+
+t_piAssetUploadToFW
