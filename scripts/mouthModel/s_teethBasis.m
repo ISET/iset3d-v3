c939c06ed52e1328c5d864f771a9afba0a511ae3
ZhengLyu <zhenglyu@stanford.edu>
2020-07-16 11:03:31 -0700

    Change piLightWrite function for spectrum scaling

--- scripts/mouthModel/s_teethBasis.m
+++ scripts/mouthModel/s_teethBasis.m
@@ -16,6 +16,8 @@ ylim([-0.8 0.6])
 teethRefl = ieReadSpectra('TeethReflectanceMeasurements_8subjects',...
                 wave, extrapVal);
 
+
+
 %% Calculate weights
 % The first 8 samples are meaningful
 wgtsPred = zeros(size(teethRefl, 2)/2, 3);
@@ -25,6 +27,7 @@ for ii=1:size(teethRefl, 2)/2
 end
 
 %% Evaluate the reconstruction
+
 teethReflPred = teethBasis * wgtsPred';
 
 %% Compare
@@ -35,6 +38,26 @@ rows = 2; cols = 4;
 for ii =1:size(teethRefl, 2)/2
     subplot(rows, cols, ii);
     plot(wave, teethRefl(:,ii), 'r', wave, teethReflPred(:,ii), 'b');
+    legend('Measured reflectance', 'Estimated reflectance')
+    ylim([0 1])
+end
+
+
+%% Load teeth reflectances
+teethRefl = ieReadSpectra('TeethReflectanceMeasurements_8subjects',...
+                wave, extrapVal);
+%% basis analysis on other measurements
+wave = 365:5:705;
+[tBasis, wgts] = basisAnalysis(teethRefl, wave, 'vis', true);
+
+recRefl = tBasis * wgts;
+
+%%
+ieNewGraphWin;
+rows = 2; cols = 4;
+for ii =1:size(recRefl, 2)/2
+    subplot(rows, cols, ii);
+    plot(wave, recRefl(:,ii), 'r', wave, teethRefl(:,ii), 'b');
     legend('Measured reflectance', 'Estimated reflectance')
     ylim([0 1])
 end
