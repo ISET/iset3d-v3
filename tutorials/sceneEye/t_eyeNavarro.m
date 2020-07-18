f330dbe5f0487494daf5649edff87b885bdf5a96
Brian Wandell <wandell@stanford.edu>
2020-07-17 22:42:00 -0700

    Simplifications and comments

--- tutorials/sceneEye/t_eyeNavarro.m
+++ tutorials/sceneEye/t_eyeNavarro.m
@@ -3,9 +3,12 @@
 % We recommend you go through t_eyeIntro.m before running
 % this tutorial.
 %
-% This tutorial renders a "letters at depth" as an example that we show of
-% the Navarro eye model.
+% This tutorial renders the PBRT SCENE "letters at depth" using the
+% Navarro eye model.  The purpose of the script is to illustrate how to
 %
+%   * set up a sceneEye with the Navarro model
+%   * position the camera and look at a particular scene object
+%   * render with chromatic aberration
 %
 % Depends on: 
 %    ISETBio, ISET3d, Docker
@@ -25,58 +28,60 @@ end
 ieInit;
 if ~piDockerExists, piDockerConfig; end
 
+%% Here are the World positions of the letters in the scene
+
+% The units are in meters
+toA = [-0.0486     0.0100     0.5556];
+toB = [  0         0.0100     0.8333];
+toC = [ 0.1458     0.0100     1.6667];
+
 %% Show the scene
 
 % This is rendered using a pinhole so the rendering is fast.  It has
 % infinite depth of field (no focal distance).
 thisEye = sceneEye('letters at depth');
 
-% Position the eye somewhere I kind of like
+% Position the eye off to the side so we can see the 3D easily
 from = [0.25,0.3,-1.3];
 thisEye.set('from',from);
 
-% Here are the World positions of the letters
-toA = [-0.0486     0.0100     0.5556];
-toB = [  0         0.0100     0.8333];
-toC = [ 0.1458     0.0100     1.6667];
-
-% Look at the position with the 'B'
+% Look at the position with the 'B'.  The values for each of the letters
+% are included above.
 thisEye.set('to',toB);
 
-% This is the distance to the edge in the scene.  We will match the focal
-% plane.
-sprintf('Mean depth %f (m)\n',mean(thisEye.get('depth range')))  
-
+% Have a quick check with the pinhole
 thisEye.set('use pinhole',true);
+
+% Given the distance from the scene, this FOV captures everything we want
 thisEye.set('fov',15);             % Degrees
 
-% The scene has a little imperfection.  We will fix it some day.
+% Render the scene
 scene = thisEye.render;
+
 sceneWindow(scene);   
 
 thisEye.summary;
 
-% Have a look at the depth map so you can see the distance to the different
-% letters.    from: (0,0.1,0):      C: 1.66, B:0.832  A:0.548
-% alternative from: (0.25,0.3,-1.3): C: 3.0   B:2.5    A:1.89
-scenePlot(scene,'depth map');
+% You can see the depth map if you like
+%   scenePlot(scene,'depth map');
 
-%% Now use the optics model.
+%% Now use the optics model with chromatic aberration
 
 % Turn off the pinhole.  The model eye (by default) is the Navarro model.
 thisEye.set('use pinhole',false);
 
-% Suppose you are in focus at the proper distance to the edge. And we turn
-% on chromatic aberration.  That will slow down the calculation, but makes
-% it more accurate and interesting.  We only use 8 spectral bands for
-% speed.  You can use up to 31.
+% We turn on chromatic aberration.  That slows down the calculation, but
+% makes it more accurate and interesting.  We often use only 8 spectral
+% bands for speed and to get a rought sense. You can use up to 31.  It is
+% slow, but that's what we do here because we are only rendering once. When
+% the GPU work is completed, this will be fast!
 nSpectralBands = 31;
 thisEye.set('chromatic aberration',nSpectralBands);
 
 % Find the distance to the object
 oDist = thisEye.get('object distance');
 
-% This is the distance to the A we calculated above
+% This is the distance to the B and we set our accommodation to that.
 thisEye.set('focal distance',oDist);  
 
 % Reduce the rendering noise by using more rays. 
@@ -91,30 +96,8 @@ thisEye.summary;
 % This takes longer than the pinhole rendering, so we do not bother with
 % the depth.
 oi = thisEye.render('render type','radiance');
+
+% Have a look.  Lots of things you can plot in this window.
 oiWindow(oi);
 
 %% END
-
-%{
-A position ans =
-
-   -0.0486
-    0.0100
-    0.5556
-
-B position
-ans =
-
-         0
-    0.0100
-    0.8333
-
-thisEye.recipe.assets.groupobjs(6).position
-
-ans =
-
-    0.1458
-    0.0100
-    1.6667
-
-%}
