445190b9daf5e7a0c319a0ab01dc96856e6f67e1
David Brainard <brainard@psych.upenn.edu>
2020-10-28 10:34:47 -0400

    Comments.

--- tutorials/introduction/Contents.m
+++ tutorials/introduction/Contents.m
@@ -3,8 +3,13 @@
 % Tutorial scripts that introduce iset3d. These are the introductory scripts, the place
 % to start. The order below is a reasonable one to go through them in.
 %
-% t_piIntro_start               - Show how to render a diffuse sphere through a pinhole camera.
-% t_piIntro_teapot              - Show how to render a scene more interesting than a sphere.
-% t_piIntro_fisheylens          - Show how to render through a lens.
-% t_piIntro_light               - Show how to manipulate scene lighting.
-% t_piIntro_cameramotion        - Show hot render with camera motion blur.
+% t_piIntro_start               - Render a diffuse sphere through a pinhole camera.
+% t_piIntro_teapot              - Render a scene more interesting than a sphere.
+% t_piIntro_macbeth             - Render a MacBeth Color Checker Chart with
+%                                   depth and illumination maps.
+% t_piIntro_macbeth_zmap        - Render a MacBeth Color Checker Chart with zmap.
+% t_piIntro_fisheyelens         - Render through a lens.
+% t_piIntro_light               - Manipulate scene lighting.
+% t_piIntro_cameramotion        - Render with camera motion blur.
+%
+% t_piIntro_rdt                 - Use RemoteDataToolbox to download and render a scene.
