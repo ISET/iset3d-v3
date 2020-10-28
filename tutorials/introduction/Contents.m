26913babd07badd0dc43eefff9f326fbb71ec3a1
David Brainard <brainard@psych.upenn.edu>
2020-10-28 09:21:46 -0400

    Comments

--- tutorials/introduction/Contents.m
+++ tutorials/introduction/Contents.m
@@ -4,5 +4,7 @@
 % to start. The order below is a reasonable one to go through them in.
 %
 % t_piIntro_start               - Show how to render a diffuse sphere through a pinhole camera.
+% t_piIntro_teapot              - Show how to render a scene more interesting than a sphere.
 % t_piIntro_fisheylens          - Show how to render through a lens.
-% t_piIntro_light               - Show how to manipulate scene lighting
+% t_piIntro_light               - Show how to manipulate scene lighting.
+% t_piIntro_cameramotion        - Show hot render with camera motion blur.
