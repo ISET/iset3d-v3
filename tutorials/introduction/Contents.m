e5cc7b7bb44dee062bfabb895cf3f9a39711b3cf
David Brainard <brainard@psych.upenn.edu>
2020-11-01 12:06:12 -0500

    Updated Contents.m

--- tutorials/introduction/Contents.m
+++ tutorials/introduction/Contents.m
@@ -10,6 +10,9 @@
 % t_piIntro_macbeth_zmap        - Render a MacBeth Color Checker Chart with zmap.
 % t_piIntro_fisheyelens         - Render through a lens.
 % t_piIntro_light               - Manipulate scene lighting.
+% t_piIntro_simpletransforms    - Show how to rotate and translate an object.
+% t_piIntro_material            - Show how to change object material
+% t_piIntro_material_color      - Show how to change object color.
 %
 % t_piIntro_cameramotion        - Render with camera motion blur.
 % t_piIntro_assetmotion         - Render with motion blur of one object.
