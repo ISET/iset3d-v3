10c3259f93a563b8276b3d09b90ba4d93aed7163
David Brainard <brainard@psych.upenn.edu>
2020-11-01 11:54:18 -0500

    Add scaling and camera position control.

--- tutorials/introduction/t_piIntro_simpletransforms.m
+++ tutorials/introduction/t_piIntro_simpletransforms.m
@@ -1,4 +1,4 @@
-%% Illustrate object rotation, translation, and scaling
+%% Illustrate object rotation, translation, and scaling, and moving camera.
 %
 % Description:
 %    This tutorial shows how you can rotate and translate an object using
@@ -13,6 +13,8 @@
 %              dhb  The only asset in the scene is called 'root', not
 %                   'Cube'.  Turns out we should be looking in the
 %                   groupobjs field, not the root field.
+%              dhb  Added scaling (based on Amy Ni's example) and camera
+%                   position changes.
 
 %% Initialize 
 clear; close all; ieInit;
@@ -72,7 +74,7 @@ sceneWindow;
 % Move cube 15 cm along positive x-axis
 for ii = 1:length(thisR.assets.groupobjs)
     if strcmp(thisR.assets.groupobjs(ii).name,'Cube')
-        % The rotation is stored in angle-axis format, along the columns.
+        % Do this by adjusting position
         thisR.assets.groupobjs(ii).position(1) = ...
             thisR.assets.groupobjs(ii).position(1) + 0.15;
     end
@@ -84,7 +86,7 @@ piWrite(thisR);
 ieAddObject(scene);
 sceneWindow;
 
-%% Scale along x-axis
+%% Scale cube along x-axis
 for ii = 1:length(thisR.assets.groupobjs)
     if strcmp(thisR.assets.groupobjs(ii).name,'Cube')
         % The rotation is stored in angle-axis format, along the columns.
@@ -98,3 +100,41 @@ piWrite(thisR);
 [scene, result] = piRender(thisR,'version',3);
 ieAddObject(scene);
 sceneWindow;
+
+%% Move camera further away
+%
+% This works if we set object distance, but not if
+% we set camera position directly.
+distance = thisR.get('object distance');
+thisR.set('object distance',distance*2);
+
+% Write and render
+piWrite(thisR);
+[scene, result] = piRender(thisR,'version',3);
+ieAddObject(scene);
+sceneWindow;
+
+%% Control full camera position in space.
+%
+% The camera position and direction are controlled by specifying where it
+% is ('from') and where it is pointed ('to').
+from = thisR.get('from');
+thisR.set('from',from + [-5 -5 0]);
+
+% Write and render
+piWrite(thisR);
+[scene, result] = piRender(thisR,'version',3);
+ieAddObject(scene);
+sceneWindow;
+
+%% Change where camera is pointed
+thisR.set('from',from);
+to = thisR.get('to');
+thisR.set('to',to + [-0.1 -0.1 1]);
+
+% Write and render
+piWrite(thisR);
+[scene, result] = piRender(thisR,'version',3);
+ieAddObject(scene);
+sceneWindow;
+
