98cfc6bc21190122314d43a7ca7a80b81ad8a5af
ZhengLyu <zhenglyu@stanford.edu>
2020-12-10 12:16:06 -0800

    added slanted bar scene and get set tutorial for asset

---
+++ tutorials/assets/t_assetsGetSet.m
@@ -0,0 +1,7 @@
+% t_assetsGetSet
+% The desired style should be 
+% thisR.set('asset object', 'name', 'material', 'value')
+% thisR.set('asset light', 'name', 'field')
+% thisR.set('asset branch', 'name', 'position', 'value')
+% Leaves can be objects or lights, branches are place holder for
+% transforms.
