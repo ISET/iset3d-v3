5ed1ab28dc37c2417b6dad6d04a43d9305ac66b3
ZhengLyu <zhenglyu@stanford.edu>
2020-12-30 14:16:18 -0800

    several modifications for cornell box project

--- utilities/recipe/piRecipeDefault.m
+++ utilities/recipe/piRecipeDefault.m
@@ -133,6 +133,15 @@ switch ieParamFormat(sceneName)
             if ~exist(fname, 'file'), error('File not found'); end
         end
         exporter = 'C4D';
+    case 'macbethcheckercb'
+        sceneName = 'mccCB';
+        FilePath = fullfile(piRootPath,'data','V3', 'MacBethCheckerCB');
+        fname = fullfile(FilePath,[sceneName,'.pbrt']);
+        if ~exist(fname,'file')
+            ieWebGet('resourcename', sceneName, 'resourcetype', 'pbrt', 'op', 'fetch', 'unzip', true);
+            if ~exist(fname, 'file'), error('File not found'); end
+        end
+        exporter = 'C4D'; 
     case 'simplescene'
         sceneName = 'SimpleScene';
         FilePath = fullfile(piRootPath,'data','V3',sceneName);
