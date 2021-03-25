ba1e10acd6c97b479fa1f66b3cfa27c06bda2e67
Brian Wandell <wandell@stanford.edu>
2021-03-24 20:50:41 -0700

    Update t_schematicEyeModels.m

--- tutorials/sceneEye/t_schematicEyeModels.m
+++ tutorials/sceneEye/t_schematicEyeModels.m
@@ -11,9 +11,13 @@ clear; close all;
 
 %% Load up a scene
 
+thisSE = sceneEye('numbersAtDepth','human eye','Navarro');
+thisSE.set('mmUnits', true);
+
+%{
 % Three letters on a checkerboard background. A is at 1.4 dpt, B is at 1
 % dpt, and C is at 0.6 dpt. 
-thisScene = sceneEye('lettersAtDepth',...
+thisSE = sceneEye('lettersAtDepth',...
                     'Adist',1/1.4,...
                     'Bdist',1/1,...
                     'Cdist',1/0.6,...
@@ -22,34 +26,36 @@ thisScene = sceneEye('lettersAtDepth',...
                     'nchecks',[128 64]);
 
 % Shrink the size of the letters so we can drop the FOV
-for ii = 1:length(thisScene.recipe.assets)
-    if (strcmp(thisScene.recipe.assets(ii).name,'A') || ...
-       strcmp(thisScene.recipe.assets(ii).name,'B') || ...
-       strcmp(thisScene.recipe.assets(ii).name,'C'))
-        thisScene.recipe.assets(ii).scale = [0.5;0.5;0.5];
+for ii = 1:length(thisSE.recipe.assets)
+    if (strcmp(thisSE.recipe.assets(ii).name,'A') || ...
+       strcmp(thisSE.recipe.assets(ii).name,'B') || ...
+       strcmp(thisSE.recipe.assets(ii).name,'C'))
+        thisSE.recipe.assets(ii).scale = [0.5;0.5;0.5];
     end
 end
+%}
 
+%%
 % A small FOV is required to see the difference between the models.
-thisScene.fov = 5; 
+thisSE.set('fov',40); 
 
 % Set general parameters
-thisScene.resolution = 128;
-thisScene.numRays = 256;
-thisScene.numCABands = 8;
+thisSE.set('spatial resolution',[128,128]);
+thisSE.set('rays per pixel',128);
+thisSE.set('chromatic aberration',8);
 
 %% Try the Navarro eye model
 
 % This tell isetbio which model to use.
-thisScene.modelName = 'Navarro';
+thisSE.set('model name','Navarro');
 
 % The Navarro model has accommodation, but let's set it to infinity for now
 % since other models may not have accommodation modeling.
-thisScene.accommodation = 0;
+thisSE.set('accommodation',0);
 
 % Render!
-thisScene.name = 'navarro'; % The name of the optical image
-oiNavarro = thisScene.render();
+thisSE.set('name','oi navarro'); % The name of the optical image
+oiNavarro = thisSE.render();
 
 % Show the retinal image
 % Everything is very out of focus since the accommodation is set to
@@ -64,19 +70,19 @@ oiWindow(oiNavarro);
 %% Try the Gullstrand-LeGrand Model
 
 % The gullstrand has no accommodation modeling. 
-thisScene.modelName = 'LeGrand';
-thisScene.name = 'LeGrand'; % The name of the optical image
-oiLeGrand = thisScene.render();
+thisSE.modelName = 'LeGrand';
+thisSE.name = 'LeGrand'; % The name of the optical image
+oiLeGrand = thisSE.render();
 oiWindow(oiLeGrand);
 
 %% Try Arizona eye model
 
-thisScene.modelName = 'Arizona';
-thisScene.accommodation = 0;
+thisSE.modelName = 'Arizona';
+thisSE.accommodation = 0;
 
 % Render!
-thisScene.name = 'arizona'; % The name of the optical image
-oiArizona = thisScene.render();
+thisSE.name = 'arizona'; % The name of the optical image
+oiArizona = thisSE.render();
 oiWindow(oiArizona);
 
 %%
