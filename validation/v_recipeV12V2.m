d24987dc902bd9057dd22a80043923d6139a926a
Brian Wandell <wandell@stanford.edu>
2021-04-10 14:49:55 -0700

    Update v_recipeV12V2.m

--- validation/v_recipeV12V2.m
+++ validation/v_recipeV12V2.m
@@ -3,15 +3,28 @@
 % This mainly tests converting from Version 1 to Version 2
 %
 
-% Load a Version 1 json file for the SimpleScene and update to Version 2.
-% The json file is stored in data/recipeV1 for validation purpose.
-%
-thisR = piJson2Recipe('simpleScene.json');
+%% Load a Version 1 json file for the SimpleScene and update to Version 2.
 
-%% Copy the Simple Scene into the local directory
 
 %% Replace the default recipe with this recipe
 
+tmp = piRecipeDefault('scene name','SimpleScene');
+piWrite(tmp);
+scene = piRender(tmp);
+sceneWindow(scene);
+
+%% Adjust the other PBRT file locations
+
+% The json file is stored in data/recipeV1 for validation purpose.
+thisR = piJson2Recipe('SimpleScene.json');
+
+thisR.set('input File',tmp.inputFile);
+thisR.set('output File',tmp.outputFile);
+
 %% Render
+
+piWrite(thisR);
+[scene, result]= piRender(thisR,'render type','radiance');
+sceneWindow(scene);
 
 %% END
