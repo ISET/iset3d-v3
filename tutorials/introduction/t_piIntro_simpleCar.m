e980725fdbaeefc21a61cf15fb6a844075a62f52
ZhengLyu <zhenglyu@stanford.edu>
2021-06-01 15:21:09 -0700

    Fixing simpleCar

--- tutorials/introduction/t_piIntro_simpleCar.m
+++ tutorials/introduction/t_piIntro_simpleCar.m
@@ -24,7 +24,7 @@
 
 ieInit;
 if ~piDockerExists, piDockerConfig; end
-if ~piScitranExists, error('scitran installation required'); end
+% if ~piScitranExists, error('scitran installation required'); end
 
 %% Render cars on a planar surface
 
