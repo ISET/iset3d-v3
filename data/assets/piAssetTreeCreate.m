d6c1f4e2d7e0de302f3818c6d754b488103415a9
Brian Wandell <wandell@stanford.edu>
2021-06-28 14:43:47 -0700

    Fleshed out method for simple geometry node creation
    When we have multiple geometry nodes on the path, we need to know whether those nodes have only 1 or more children.  That is next.

--- data/assets/piAssetTreeCreate.m
+++ data/assets/piAssetTreeCreate.m
@@ -67,16 +67,37 @@ wpos    = flatR.get('asset',surfaceName,'world position')
 wscale  = flatR.get('asset',surfaceName,'world scale')
 wrotate = flatR.get('asset',surfaceName,'world rotation angle')
 
+% How many geometry nodes (branches) are from the object to the root?
+% All the nodes up the path are geometry nodes.  Object nodes are
+% always leafs.
 id = flatR.get('asset',surfaceName,'path to root');
+fprintf('Geometry nodes:  %d\n',numel(id) - 1);
+
 for ii=2:numel(id)
     flatR.set('asset',id(ii),'delete');
 end
 
-flatR.set('asset',surfaceName,'world position',wpos);
-pid = flatR.get('asset parent id',surfaceName);
-piAssetSet(flatR, pid, 'scale',wscale);
-rotMatrix = [wrotate; fliplr(eye(3))]
-piAssetSet(flatR, pid, 'rotation', rotMatrix);
+% Check again
+id = flatR.get('asset',surfaceName,'path to root');
+fprintf('Geometry nodes:  %d\n',numel(id) - 1);
+
+if (numel(id)-1 == 0)
+    geometryNode = piAssetCreate('type','branch');
+    geometryNode.name = '001_Cube_G';
+    flatR.set('asset','root','add',geometryNode);
+    flatR.set('asset',surfaceName,'parent',geometryNode.name);
+end
+
+piAssetSet(flatR, geometryNode.name, 'translate',wpos);
+piAssetSet(flatR, geometryNode.name, 'scale',wscale);
+rotMatrix = [wrotate; fliplr(eye(3))];
+piAssetSet(flatR, geometryNode.name, 'rotation', rotMatrix);
+
+% flatR.set('asset',surfaceName,'world position',wpos);
+% pid = flatR.get('asset parent id',surfaceName);
+% piAssetSet(flatR, pid, 'scale',wscale);
+% rotMatrix = [wrotate; fliplr(eye(3))]
+% piAssetSet(flatR, pid, 'rotation', rotMatrix);
 
 [scene, results] = piWRS(flatR);
 
