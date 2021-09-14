983a01f12ef292c0fa4ab0413df7dcd08953200c
Thomas Goossens <thomas42@vistalab-compute-1.c.som-vistalab.internal>
2021-09-14 14:21:46 -0700

    Create PSF script

--- scripts/psf/s_psfCreate.m
+++ scripts/psf/s_psfCreate.m
@@ -1,30 +1,69 @@
 
 %% Init
+clear;
 ieInit;
 if ~piDockerExists, piDockerConfig; end
 
 %% Check that we have a scene at all
 
-pa = piAssetLoad('pointarray512');
-thisR = pa.thisR;
-thisR.outputFile='/home/thomas/Documents/stanford/libraries/iset/iset3d/local/flatSurface/flatSurface.pbrt'
-piAssetGeometry(thisR);
-%piWRS(thisR);
+
 
 %%  Adjust the field of view and other parameters
 
 lensfile  = 'dgauss.22deg.12.5mm.json';    % 30 38 18 10
+lensfile  = 'dgauss.22deg.3.0mm.json';    % 30 38 18 10
+rtffile  = 'dgauss.22deg.3.0mm.json';    % 30 38 18 10
 fprintf('Using lens: %s\n',lensfile);
-thisR.camera = piCameraCreate('omni','lensFile',lensfile);
 
-thisR.set('spatial resolution',[2000 2000]);
-thisR.set('rays per pixel',32);
 
-thisR.set('asset','pointarray_512_64-1712','scale',[3 3 1]);
+thisDocker = 'vistalab/pbrt-v3-spectral:raytransfer-spectral';
+
+%% Add a lens and render.
+%camera = piCameraCreate('omni','lensfile','dgauss.22deg.12.5mm.json');
+cameraOmni = piCameraCreate('omni','lensfile','dgauss.22deg.3.0mm_aperture0.6_spectral.json')
+cameraOmni.filmdistance.type='float'
+cameraOmni.filmdistance.value=0.002167;
+cameraOmni = rmfield(cameraOmni,'focusdistance')
+cameraOmni.aperturediameter.value=0.6
+
+
+cameraRTF = piCameraCreate('raytransfer','lensfile','dgauss.22deg.3.0mm.json-raytransfer-spectral.json')
+cameraRTF.aperturediameter.value=0.6
+cameraRTF.aperturediameter.type='float'
+
+cameras={cameraOmni,cameraRTF};oiLabels={'cameraOmni','cameraRTF'}
+
+
+
+
+for c=1:numel(cameras)
+pa = piAssetLoad('pointarray512');
+
+
+thisR = pa.thisR;
+thisR.outputFile='/home/thomas42/Documents/MATLAB/iset3d/local/flatSurface/flatSurface.pbrt'
+piAssetGeometry(thisR);
+%piWRS(thisR);
+
+
+thisR.camera = cameras{c};
+
+
+thisR.set('spatial resolution',[5000 5000]);
+thisR.set('rays per pixel',300);
+
+thisR.set('asset','pointarray_512_64-1712','scale',[5 5 1]);
+
+
+thisR.integrator.subtype='path'
+thisR.integrator.numCABands.type = 'integer';
+thisR.integrator.numCABands.value =1
 
 tic;
-piWRS(thisR);
+oi{c}=piWRS(thisR,'render type','radiance','dockerimagename',thisDocker);
+oi{c}.name=oiLabels{c};
 toc;
+end
 
 thisR.show('objects');
 
---
+++ scripts/psf/visualizePsf.m
@@ -0,0 +1,8 @@
+%%oo=load("oiRTF5000.mat")
+
+
+
+data=oi{1}.data.illuminance,
+
+
+
