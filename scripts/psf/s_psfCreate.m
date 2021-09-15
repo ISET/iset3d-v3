23cfad66bd87e271320fc6a704a98637ff0348ab
Brian Wandell <wandell@stanford.edu>
2021-09-15 16:34:43 -0700

    Update s_psfCreate.m

--- scripts/psf/s_psfCreate.m
+++ scripts/psf/s_psfCreate.m
@@ -31,13 +31,20 @@ oiLabels = {'cameraOmni','cameraRTF'};
 
 % The scene is just the point array on the flat surface
 pa = piAssetLoad('pointarray512');
-pa.thisR.show('objects');
-piAssetSet(pa.thisR,pa.mergeNode,'translate',[0 0 8]);
 
-pa.thisR.show;
+thisR = pa.thisR;
 
-piWRS(pa.thisR)
-piAssetGeometry(pa.thisR);
+thisR.show('objects');
+piAssetSet(thisR,pa.mergeNode,'translate',[0 0 2]);
+
+%% Try a camera now
+
+thisR.set('camera',cameraOmni);   
+thisR.set('spatial resolution',300*[1 1]);
+thisR.set('rays per pixel',64);
+thisR.set('focus distance',100);    % In meters
+thisR.set('film diagonal',4);
+piWRS(thisR);
 
 %%
 %thisR.set('asset','pointarray_512_64-1712','scale',[5 5 1]);
@@ -46,52 +53,46 @@ piAssetGeometry(pa.thisR);
 %thisR.set('asset','face-2425','worldtranslate',[0 0 -3]); % Put closer STILL GETS IMAGED???
 %thisR.set('asset','face-2425','translate',[0 0 -100]); % Put closer STILL GETS IMAGED???
 
+
 thisR.show('objects');
 
 % thisR.outputFile= '/home/thomas42/Documents/MATLAB/iset3d/local/flatSurface/flatSurface.pbrt'
 piWrite(thisR)
 piAssetGeometry(thisR);
 
-%%
+%% Compare the two cameras
+
+
 for c=1:numel(cameras)
-
-
-
-
-
-
-%piWRS(thisR);
-
-
-thisR.camera = cameras{c};
-
-
-thisR.set('spatial resolution',5000*[1 1]);
-thisR.set('rays per pixel',10);
-
-
-thisR.show('objects');
-
-thisR.integrator.subtype='path'
-thisR.integrator.numCABands.type = 'integer';
-thisR.integrator.numCABands.value =1
-
-tic;
-piWrite(thisR);
-pause(10)
-disp('start render')
-[obj,results] = piRender(thisR,...
-    'docker image name',thisDocker, ...
-    'render type','radiance');
-oi{c}=obj;
-
-close all
-
-oi{c}.name=oiLabels{c};
-
-disp('Waiting for interaction')
-pause
-toc;
+    %piWRS(thisR);
+    
+    thisR.camera = cameras{c};
+    
+    
+    thisR.set('spatial resolution',5000*[1 1]);
+    thisR.set('rays per pixel',10);
+    
+    
+    thisR.show('objects');
+    
+    thisR.integrator.subtype = 'path';
+    thisR.integrator.numCABands.type = 'integer';
+    thisR.integrator.numCABands.value = 1;
+    
+    tic;
+    piWrite(thisR);
+    pause(10)
+    disp('start render')
+    [obj,results] = piRender(thisR,...
+        'docker image name',thisDocker, ...
+        'render type','radiance');
+    oi{c}=obj;
+    
+    close all
+    
+    oi{c}.name=oiLabels{c};
+    
+    toc;
 end
 
 %%
