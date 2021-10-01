34c0f0e96c9b10ade61f75aec3e8d608e2507627
Brian Wandell <wandell@stanford.edu>
2021-10-01 10:55:57 -0700

    Small edits.  Maybe parameter value changes.

--- scripts/psf/isetLensPSF.m
+++ scripts/psf/isetLensPSF.m
@@ -1,56 +1,66 @@
+%%
 clear;
- close all
+close all
+ 
 %% Choose lens and sampling of lens  aperture
 
 lensName = 'dgauss.22deg.50.0mm_aperture6.0.json';
 
-lens=lensC('file',lensName)
-lens.apertureSample=1000*[1 1 ];
-
+lens = lensC('file',lensName);
+lens.apertureSample=400*[1 1 ];   % 1000 is OK, 4000 is a lot.
 
 %% Helper variables
 lensThickness = lens.surfaceArray(1).sRadius-lens.surfaceArray(1).sCenter(3);
 
-
+%%
+fieldHeightY_mm = 0;
+objectFromFront = 3000;   % For zemax, measures from first lens vertex
+objectFromRear = objectFromFront + lensThickness; % For isetlens
+objectFromFilm = objectFromRear+filmdistance_mm;
  
-%% Determine required film distance
-% Define onject distance from rear lens vertx
-% This calculates the image point
-
-objectDistance_fromrear=3000+lensThickness
-
-
-im=lens.findImagePoint([0 0 -objectDistance_fromrear],1,1);
-
-% Distance from the film
-filmdistance_mm=im(1,3) 
-
-% Manually override if wanted ff
 filmdistance_mm =  36.959;
 
-
-%% Calculqte PSF
+%% Calculate PSF
 
 
 % Define point source for which PSF needs to be calculated
-ps = {[0 0 -objectDistance_fromrear]}
+ps = {[0 0 -objectFromRear]}
 
 
 % Define psfCameraC
-wave = linspace(500,1000,7); sz = [0.02,0.02]; pos = [0 0 filmdistance_mm]; res = [200 200];
-film = filmC('position', pos, 'size', sz, 'wave', wave, 'resolution', res);
+wave = linspace(500,1000,7); 
+filmSize = [0.04,0.04]; % mm
+pos = [0 0 filmdistance_mm];  % distance
+res = [200 200];   % Pixel samples
+film = filmC('position', pos, 'size', filmSize, 'wave', wave, 'resolution', res);
 psfCamera = psfCameraC('lens',lens,'film',film,'pointsource',ps);
 
 
-% Calculate psf imagem
+%% Calculate psf image
 % nlines is how many lines you want to see plotted while tracing
-psfCamera.estimatePSF('nLines',10,'jitter',false,'diffractionmethod','HURB');
-oimage = psfCamera.oiCreate();  % vcAddObject(oi); oiWindow;
+psfCamera.estimatePSF('nLines',0,'jitter',false);
 
-% Extract the point spread data
-x_micron = 1e3*linspace(-sz(1)/2,sz(1)/2,res(1));y=x_micron;
+% Turn this into ISETCam optical image
+oimage = psfCamera.oiCreate();  % vcAddObject(oi); oiWindow;
+% oiWindow(oimage);
+
+%% Extract the point spread data
+x_micron = 1e3*linspace(-filmSize(1)/2,filmSize(1)/2,res(1));
+y = x_micron;
 PSF = oiGet(oimage, 'photons',wave(1));
-figure(5);clf;imagesc(x_micron,y,PSF)
+LSF = sum(PSF,1);
+ieNewGraphWin; plot(x_micron,LSF)
+
+load('zemax_lsf_3000.mat','zemax');
+ieNewGraphWin; plot(zemax(:,1),zemax(:,2));
+
+ieNewGraphWin; 
+plot(x_micron,LSF/max(LSF(:)), 'r--',zemax(:,1),zemax(:,2)/max(zemax(:,2)),'g-')
+grid on
+legend({'isetlens','zemax'});
+
+%%
+ieNewGraphWin;clf;imagesc(x_micron,y,PSF)
 shading interp
 xlabel('x (\mu m)')
 ylabel('x (\mu m)')
--- scripts/psf/paperFigures/compareFieldHeights.m
+++ scripts/psf/paperFigures/compareFieldHeights.m
@@ -1,120 +1,120 @@
+%% Script to make the PSF figures
+
 clear; close all
 %% My own implementation for Geometric PSF calculation
 
 filmdistance_mm=36.959;
-lens=lensC('file','dgauss.22deg.50.0mm_aperture6.0.json')
+lens = lensC('file','dgauss.22deg.50.0mm_aperture6.0.json')
 lensThickness = lens.surfaceArray(1).sRadius-lens.surfaceArray(1).sCenter(3);
-addPlane=outputPlane(filmdistance_mm); % Film plane
+addPlane = outputPlane(filmdistance_mm); % Film plane
 lens = addPlane(lens);
 
 %% Load RTF
-fit=load('rtf-dgauss.22deg.50mm.mat','fit');
-rtf=fit.fit{1};
-
-
-
+fit = load('rtf-dgauss.22deg.50mm.mat','fit');
+rtf = fit.fit{1};
 
 %% Configurations
 fieldHeights_mm = [0 1000 0 1000 0]
-objectFromFront_distances_mm = [3000 3000 3300 3300 800 ]; 
+objectFromFront_distances_mm = [3000 3000 3300 3300 800 ];
 assert(numel(fieldHeights_mm)==numel(objectFromFront_distances_mm))
 
+%%
 for f=1:numel(fieldHeights_mm)
     
-fieldHeightY_mm = fieldHeights_mm(f);
-objectFromFront = objectFromFront_distances_mm(f);   % For zemax, measures from first lens vertex
-objectFromRear= objectFromFront+lensThickness; % For isetlens
-objectFromFilm= objectFromRear+filmdistance_mm; 
-
-
-%% Grid definition to sample the pupil uniformly
-gridCenterZ = -lensThickness;
-nbGridPoints= 2000;
-gridSize_mm = 30; 
-gridpoints = linspace(-gridSize_mm/2,gridSize_mm/2,nbGridPoints);
-
-[rows,cols] = meshgrid(gridpoints,gridpoints);
-
-grid=[rows(:) cols(:) ones(numel(rows),1)*gridCenterZ];
-
-
-%% Generate collection of rays by tracing origin points to grid points
-clear origins directions;
-origins = repmat([0 fieldHeightY_mm -objectFromRear],[numel(rows) 1]);
-directions = (grid-origins);
-directions = directions./sqrt(sum(directions.^2,2));
-
-
-%% Trace through real lens
-waveindex=1;
-waveIndices=waveindex*ones(1, size(origins, 1));
-rays = rayC('origin',origins,'direction', directions, 'waveIndex', waveIndices, 'wave', lens.wave);
-[~, ~, pOut, pOutDir] = lens.rtThroughLens(rays, rays.get('n rays'), 'visualize', false);
-lensFilmPos(:,:,f)=pOut;
-
-%% Trace through RTF lens
-%%% Trace ray to input plane
-  
-rtfFilmPos(:,:,f)=rtfTraceObjectToFilm(rtf,origins,directions,filmdistance_mm);
-
-%% Unit conversions
-mmToMicron=1e3;
-micronTomm=1e-3;
-
-
-%% Histogran binning
-nbBins=50;
-
-%% Add zero padding
-% This pads zeros to both  the sides of te LSF . This is imporant to get
-% the right MTF when using FFT, because FFT assumes the signal to ne
-% perodic. In reaslity the geometric PSF is zero everywhere except near its
-% peak. 
-nbZeroPad = 20;
-
-
-
-%% Generate LSF and MTF figures
-maxnorm = @(x)x/max(x);
-
-fig=figure(f);clf;hold on;
-fig.Name=['Object distance ' num2str(objectFromFront)  '- Field height ' num2str(fieldHeightY_mm) 'mm' ];
-
-
-labels={'X','Y'}
-for i = 1:2
-
-ax1=subplot(2,2,1+(i-1)); hold on;
-
-[binsLens_micron,countsLens]=paddedHistogram(lensFilmPos(:,i,f),nbBins,nbZeroPad)
-[binsRTF_micron,countsRTF]=paddedHistogram(rtfFilmPos(:,i,f),nbBins,nbZeroPad)
-
-
-hlens=plot(binsLens_micron,maxnorm(countsLens));
-hrtf=plot(binsRTF_micron,maxnorm(countsRTF));
-
-
-title(['Line spread function ' labels{i}])
-xlabel('Micron')
-
-
-% MTF 
-subplot(2,2,3+(i-1)); hold on;
-[freqLens,MLens]=fftMTF(micronTomm*binsLens_micron,countsLens);
-[freqRTF,Mrtf]=fftMTF(micronTomm*binsRTF_micron,countsRTF);
-hlens=plot(freqLens,MLens)
-hrtf=plot(freqRTF,Mrtf)
-
-title(['MTF' labels{i}])
-xlabel('cycles/mm')
-
-end
-
-set(findall(gcf,'-property','FontSize'),'FontSize',10);
-set(findall(gcf,'-property','interpreter'),'interpreter','latex');
-
-autoArrangeFigures
-pause(0.01)
+    fieldHeightY_mm = fieldHeights_mm(f);
+    objectFromFront = objectFromFront_distances_mm(f);   % For zemax, measures from first lens vertex
+    objectFromRear= objectFromFront+lensThickness; % For isetlens
+    objectFromFilm= objectFromRear+filmdistance_mm;
+    
+    
+    %% Grid definition to sample the pupil uniformly
+    gridCenterZ = -lensThickness;
+    nbGridPoints= 200;
+    gridSize_mm = 30;
+    gridpoints = linspace(-gridSize_mm/2,gridSize_mm/2,nbGridPoints);
+    
+    [rows,cols] = meshgrid(gridpoints,gridpoints);
+    
+    grid=[rows(:) cols(:) ones(numel(rows),1)*gridCenterZ];
+    
+    
+    %% Generate collection of rays by tracing origin points to grid points
+    clear origins directions;
+    origins = repmat([0 fieldHeightY_mm -objectFromRear],[numel(rows) 1]);
+    directions = (grid-origins);
+    directions = directions./sqrt(sum(directions.^2,2));
+    
+    
+    %% Trace through real lens using isetlens
+    % This works correctly with Zemax, and it matches the RTF
+    % calculation below.
+    %
+    % So, isetlens is OK to this part of the calculations.
+    waveindex=1;
+    waveIndices=waveindex*ones(1, size(origins, 1));
+    rays = rayC('origin',origins,'direction', directions, 'waveIndex', waveIndices, 'wave', lens.wave);
+    [~, ~, pOut, pOutDir] = lens.rtThroughLens(rays, rays.get('n rays'), 'visualize', false);
+    lensFilmPos(:,:,f)= pOut;
+    
+    %% Trace through real lens with RTF lens        
+    rtfFilmPos(:,:,f) = rtfTraceObjectToFilm(rtf,origins,directions,filmdistance_mm);
+    
+    %% Unit conversions
+    mmToMicron=1e3;
+    micronTomm=1e-3;
+    
+    
+    %% Histogran binning
+    nbBins=50;
+    
+    %% Add zero padding
+    % This pads zeros to both  the sides of te LSF . This is imporant to get
+    % the right MTF when using FFT, because FFT assumes the signal to ne
+    % perodic. In reaslity the geometric PSF is zero everywhere except near its
+    % peak.
+    nbZeroPad = 20;    
+    
+    %% Generate LSF and MTF figures
+    maxnorm = @(x)x/max(x);
+    
+    fig=figure(f);
+    clf;hold on;
+    fig.Name=['Object distance ' num2str(objectFromFront)  '- Field height ' num2str(fieldHeightY_mm) 'mm' ];
+        
+    labels={'X','Y'}
+    for i = 1:2
+        
+        ax1=subplot(2,2,1+(i-1)); hold on;
+        
+        [binsLens_micron,countsLens]=paddedHistogram(lensFilmPos(:,i,f),nbBins,nbZeroPad)
+        [binsRTF_micron,countsRTF]=paddedHistogram(rtfFilmPos(:,i,f),nbBins,nbZeroPad)
+        
+        
+        hlens=plot(binsLens_micron,maxnorm(countsLens));
+        hrtf=plot(binsRTF_micron,maxnorm(countsRTF));
+        
+        
+        title(['Line spread function ' labels{i}])
+        xlabel('Micron')
+        
+        
+        % MTF
+        subplot(2,2,3+(i-1)); hold on;
+        [freqLens,MLens]=fftMTF(micronTomm*binsLens_micron,countsLens);
+        [freqRTF,Mrtf]=fftMTF(micronTomm*binsRTF_micron,countsRTF);
+        hlens=plot(freqLens,MLens)
+        hrtf=plot(freqRTF,Mrtf)
+        
+        title(['MTF' labels{i}])
+        xlabel('cycles/mm')
+        
+    end
+    
+    set(findall(gcf,'-property','FontSize'),'FontSize',10);
+    set(findall(gcf,'-property','interpreter'),'interpreter','latex');
+    
+    autoArrangeFigures
+    pause(0.01)
 end
 
 
@@ -136,8 +136,8 @@ labels={'LSF\,X','LSF\,Y'}
 
 for i=1:2
     for f=1:nbConfigs
-    
-    
+        
+        
         index=sub2ind([nbConfigs 2 ],f,i)
         subplot(2,nbConfigs,index); hold on
         
@@ -148,33 +148,33 @@ for i=1:2
         hrtf(f)=plot(binsRTF_micron,maxnorm(countsRTF),'color',colorRTF);
         
         xlabel('Micron')
-
-      if(f==1)
-         
-       ylabel(['$\mathbf{' labels{i} '}$'])
-       ylh = get(gca,'ylabel');
-    ylp = get(ylh, 'Position');
-    ylh.FontWeight='Bold'
-    set(ylh, 'Rotation',0, 'Position',ylp, 'VerticalAlignment','middle', 'HorizontalAlignment','right')
-    
-      end
-      
-      if(f>1)
+        
+        if(f==1)
+            
+            ylabel(['$\mathbf{' labels{i} '}$'])
+            ylh = get(gca,'ylabel');
+            ylp = get(ylh, 'Position');
+            ylh.FontWeight='Bold'
+            set(ylh, 'Rotation',0, 'Position',ylp, 'VerticalAlignment','middle', 'HorizontalAlignment','right')
+            
+        end
+        
+        if(f>1)
             % no ylabel
             ax=gca; ax.YTickLabels=[];ax.YTick=[];
             ax.YAxis.Visible='off'
-      end
-      
-      if(i==1)
-          xpos=[2 4 10 10 500]
-         text(xpos(f),0.8,['Object dist.: ' num2str(objectFromFront_distances_mm(f)/1000) ' m']) 
-         text(xpos(f),0.7,['Field height: ' num2str(fieldHeights_mm(f)/1000) ' m']) 
-      end
+        end
+        
+        if(i==1)
+            xpos=[2 4 10 10 500]
+            text(xpos(f),0.8,['Object dist.: ' num2str(objectFromFront_distances_mm(f)/1000) ' m'])
+            text(xpos(f),0.7,['Field height: ' num2str(fieldHeights_mm(f)/1000) ' m'])
+        end
         
     end
     
     
-
+    
 end
 
 % Add legend
@@ -207,8 +207,8 @@ labels={'MTF\,X','MTF\,Y'}
 
 for i=1:2
     for f=1:nbConfigs
-    
-    
+        
+        
         index=sub2ind([nbConfigs 2 ],f,i)
         subplot(2,nbConfigs,index); hold on
         
@@ -220,37 +220,37 @@ for i=1:2
         [freqRTF,Mrtf]=fftMTF(micronTomm*binsRTF_micron,countsRTF);
         hlens=plot(freqLens,MLens)
         hrtf=plot(freqRTF,Mrtf)
-
+        
         xlabel('cycles/mm')
         
         
-
-      if(f==1)
-         
-       ylabel(['$\mathbf{' labels{i} '}$'])
-       ylh = get(gca,'ylabel');
-    ylp = get(ylh, 'Position');
-    ylh.FontWeight='Bold'
-    set(ylh, 'Rotation',0, 'Position',ylp, 'VerticalAlignment','middle', 'HorizontalAlignment','right')
-    
-      end
-      
-      if(f>1)
+        
+        if(f==1)
+            
+            ylabel(['$\mathbf{' labels{i} '}$'])
+            ylh = get(gca,'ylabel');
+            ylp = get(ylh, 'Position');
+            ylh.FontWeight='Bold'
+            set(ylh, 'Rotation',0, 'Position',ylp, 'VerticalAlignment','middle', 'HorizontalAlignment','right')
+            
+        end
+        
+        if(f>1)
             % no ylabel
             ax=gca; ax.YTickLabels=[];ax.YTick=[];
             ax.YAxis.Visible='off'
-      end
-      
-      if(i==1)
-          xpos=[1 1 1 1 1]*10
-         text(xpos(f),0.8,['Object dist.: ' num2str(objectFromFront_distances_mm(f)/1000) ' m']) 
-         text(xpos(f),0.7,['Field height: ' num2str(fieldHeights_mm(f)/1000) ' m']) 
-      end
+        end
+        
+        if(i==1)
+            xpos=[1 1 1 1 1]*10
+            text(xpos(f),0.8,['Object dist.: ' num2str(objectFromFront_distances_mm(f)/1000) ' m'])
+            text(xpos(f),0.7,['Field height: ' num2str(fieldHeights_mm(f)/1000) ' m'])
+        end
         
     end
     
     
-
+    
 end
 
 % Add legend
