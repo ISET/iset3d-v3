c1dd61a0fe854e6de02c4cb7e01e1090ea69758e
David Cardinal <github@cardinalphoto.com>
2021-10-28 20:48:52 -0700

    Only change paths on Windows

--- docker/pathToLinux.m
+++ docker/pathToLinux.m
@@ -1,10 +1,12 @@
 function output = pathToLinux(inputPath)
 
-if isequal(fullfile(inputPath), inputPath)
-    % assume we have a drive letter
-    output = inputPath(3:end);
-    output = strrep(output, '\','/');
-else
-    output = strrep(inputPath, '\','/');
+if ispc
+    if isequal(fullfile(inputPath), inputPath)
+        % assume we have a drive letter
+        output = inputPath(3:end);
+        output = strrep(output, '\','/');
+    else
+        output = strrep(inputPath, '\','/');
+    end
 end
 
